3b14bddf61e6b9c7790a04ab6a61a077882050f5
ZtMbam <98670757+ZtMbam@users.noreply.github.com>
2023-08-24 15:39:25 -0400

    Centered Statement
    Centered Statement Pattern

---
+++ patterns/centeredstatement.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Title: A Centered Statement
+ * Slug: twentytwentyfour/centeredstatement
+ * Categories: text
+ */
+
+?>
+
+<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:columns {"backgroundColor":"base"} -->
+<div class="wp-block-columns has-base-background-color has-background"><!-- wp:column {"width":"100%","style":{"spacing":{"padding":{"top":"40px","right":"40px","bottom":"40px","left":"40px"}}},"backgroundColor":"base"} -->
+<div class="wp-block-column has-base-background-color has-background" style="padding-top:40px;padding-right:40px;padding-bottom:40px;padding-left:40px;flex-basis:100%"><!-- wp:paragraph {"align":"center","style":{"typography":{"lineHeight":"1.3","fontStyle":"normal","fontWeight":"300","fontSize":"16px"},"spacing":{"padding":{"top":"80px","right":"80px","bottom":"80px","left":"80px"},"margin":{"top":"50px","right":"50px","bottom":"50px","left":"50px"}}},"backgroundColor":"base","fontFamily":"cardo"} -->
+<p class="has-text-align-center has-base-background-color has-background has-cardo-font-family" style="margin-top:50px;margin-right:50px;margin-bottom:50px;margin-left:50px;padding-top:80px;padding-right:80px;padding-bottom:80px;padding-left:80px;font-size:16px;font-style:normal;font-weight:300;line-height:1.3"><em>Queste fotografie non solo hanno catturato la bellezza fisica del Piva Arts &amp; Crafts Hotel, ma hanno anche trasmesso le emozioni e le sensazioni che l'hotel evoca nei suoi ospiti</em></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
