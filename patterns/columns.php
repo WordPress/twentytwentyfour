3a6b10f66322eab87568200abac3f3bfbdb01c87
Mel Choyce-Dwan <melchoyce@users.noreply.github.com>
2023-08-24 14:19:06 -0400

    Add "columns" pattern
    See #8.

---
+++ patterns/columns.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Title: Columns
+ * Slug: twentytwentyfour/columns
+ * Inserter: Yes
+ */
+
+?>
+
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"7em","right":"7em","bottom":"7em","left":"7em"}}},"backgroundColor":"base","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull has-base-background-color has-background" style="padding-top:7em;padding-right:7em;padding-bottom:7em;padding-left:7em"><!-- wp:columns {"align":"wide"} -->
+<div class="wp-block-columns alignwide"><!-- wp:column {"width":"33%","style":{"spacing":{"blockGap":"","padding":{"right":"12%"}}}} -->
+<div class="wp-block-column" style="padding-right:12%;flex-basis:33%"><!-- wp:heading {"level":3} -->
+<h3 class="wp-block-heading">We recognize the crucial role architecture plays in shaping a sustainable future.</h3>
+<!-- /wp:heading --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"style":{"spacing":{"blockGap":"5.6em"}}} -->
+<div class="wp-block-column"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:group {"layout":{"type":"flex","orientation":"vertical","justifyContent":"left"}} -->
+<div class="wp-block-group"><!-- wp:heading {"level":4,"style":{"typography":{"lineHeight":"1.1","fontSize":"22px"}},"fontFamily":"system-font"} -->
+<h4 class="wp-block-heading has-system-font-font-family" style="font-size:22px;line-height:1.1"><strong>Consulting</strong></h4>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Our vision is to be at the forefront of architectural innovation, fostering a global community of architects and enthusiasts united by a passion for creating spaces.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group"><!-- wp:heading {"level":4,"style":{"typography":{"lineHeight":"1.1","fontSize":"22px"}},"fontFamily":"system-font"} -->
+<h4 class="wp-block-heading has-system-font-font-family" style="font-size:22px;line-height:1.1"><strong><strong>Project Management</strong></strong></h4>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Our vision is to be at the forefront of architectural innovation, fostering a global community of architects and enthusiasts united by a passion for creating spaces.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group"><!-- wp:heading {"level":4,"style":{"typography":{"lineHeight":"1.1","fontSize":"22px"}},"fontFamily":"system-font"} -->
+<h4 class="wp-block-heading has-system-font-font-family" style="font-size:22px;line-height:1.1"><strong>Design</strong></h4>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Our vision is to be at the forefront of architectural innovation, fostering a global community of architects and enthusiasts united by a passion for creating spaces.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group"><!-- wp:heading {"level":4,"style":{"typography":{"lineHeight":"1.1","fontSize":"22px"}},"fontFamily":"system-font"} -->
+<h4 class="wp-block-heading has-system-font-font-family" style="font-size:22px;line-height:1.1"><strong>Maintenance</strong></h4>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Our vision is to be at the forefront of architectural innovation, fostering a global community of architects and enthusiasts united by a passion for creating spaces.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
