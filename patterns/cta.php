b61cf0d9965c8d77e4b79a4d38e26f1da5162b4d
LynUrth <lurtheil@gmail.com>
2023-08-24 15:00:52 -0400

    cta pattern
    adding a centered call to action for the twentytwentyfour theme from Wordcamp23

---
+++ patterns/cta.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Title: Centered Call To Action
+ * Slug: twentytwentyfour/cta
+ * Categories: text
+ */
+
+?>
+
+<!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"","padding":{"top":"100px","right":"100px","bottom":"100px","left":"100px"}}},"backgroundColor":"base","layout":{"type":"default"}} -->
+<div class="wp-block-group alignwide has-base-background-color has-background" style="padding-top:100px;padding-right:100px;padding-bottom:100px;padding-left:100px"><!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"50px","bottom":"50px"}}},"backgroundColor":"tertiary","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide has-tertiary-background-color has-background" style="padding-top:50px;padding-bottom:50px"><!-- wp:paragraph {"align":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|contrast"}}},"typography":{"fontSize":"28px","lineHeight":"1.6"}},"textColor":"contrast","fontFamily":"cardo"} -->
+<p class="has-text-align-center has-contrast-color has-text-color has-link-color has-cardo-font-family" style="font-size:28px;line-height:1.6">Join 900+ designers</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|contrast"}}},"typography":{"fontSize":"16px"}},"textColor":"contrast","fontFamily":"system-font"} -->
+<p class="has-text-align-center has-contrast-color has-text-color has-link-color has-system-font-font-family" style="font-size:16px">Stay in the loop with everything you need to know</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons {"align":"wide","layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons alignwide"><!-- wp:button {"width":25} -->
+<div class="wp-block-button has-custom-width wp-block-button__width-25"><a class="wp-block-button__link wp-element-button">Sign up</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
