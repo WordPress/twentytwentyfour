e65cb4031ac96ccb357951cd3b48194d4bf1c5f7
Alexey Medvedev <alexeymedve@gmail.com>
2023-08-24 11:47:05 -0400

    line-height change to 130%

--- patterns/about.php
+++ patterns/about.php
@@ -7,8 +7,8 @@
 
 ?>
 
-<!-- wp:paragraph {"align":"center","style":{"typography":{"fontStyle":"italic","fontWeight":"400","lineHeight":"1.2"}},"fontSize":"large","fontFamily":"cardo"} -->
-<p class="has-text-align-center has-cardo-font-family has-large-font-size" style="font-style:italic;font-weight:400;line-height:1.2">
+<!-- wp:paragraph {"align":"center","style":{"typography":{"fontStyle":"italic","fontWeight":"400","lineHeight":"1.3"}},"fontSize":"large","fontFamily":"cardo"} -->
+<p class="has-text-align-center has-cardo-font-family has-large-font-size" style="font-style:italic;font-weight:400;line-height:1.3">
     <?php
         /* Translators: WordPress link. */
         $wordpress_link = '<a href="' . esc_url( __( 'https://wordpress.org', 'twentytwentyfour' ) ) . '" rel="nofollow">' . esc_html__( 'Money Studies', 'twentytwentyfour' ) . '</a>';
