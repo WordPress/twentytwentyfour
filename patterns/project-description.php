c1021e127ef0e39f176b7464fdbbe802dce5a9fa
marcelle42 <karenwolf@gmail.com>
2023-08-24 14:55:13 -0400

    Create project-description.php
    Created block pattern for the Project Description pattern

---
+++ patterns/project-description.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Title: Project Description
+ * Slug: twentytwentyfour/project-description
+ * Categories: featured
+ */
+?>
+
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"75px","right":"101px","bottom":"75px","left":"101px"}}},"backgroundColor":"custom-brown","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-custom-brown-background-color has-background" style="padding-top:75px;padding-right:101px;padding-bottom:75px;padding-left:101px"><!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"style":{"spacing":{"blockGap":""}}} -->
+<div class="wp-block-column"><!-- wp:paragraph {"fontSize":"small"} -->
+<p class="has-small-font-size"><?php echo esc_html_x('Piva Hotel — Overview','Project Description pattern header','twentytwentyfour'); ?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"style":{"spacing":{"blockGap":""}}} -->
+<div class="wp-block-column"><!-- wp:paragraph {"fontSize":"medium"} -->
+<p class="has-medium-font-size"><?php echo esc_html_x('Sessione fotografica per il Piva Arts &amp; Crafts Hotel vicino al lago Piva, in Montenegro. Un lago artificiale è stato costruito nel 1975, dividendo il Piva Kanyon per costruire una diga Mratinje.','Project Description pattern text','twentytwentyfour');?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:image {"id":26,"aspectRatio":"16/9","scale":"cover","sizeSlug":"large","linkDestination":"none","className":"is-style-default"} -->
+<figure class="wp-block-image size-large is-style-default"><img src="<?php esc_url(get_template_directory_uri().'assets/small-boat-in-big-water-1024x495.jpg';?>" alt="" class="wp-image-26" style="aspect-ratio:16/9;object-fit:cover"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:group -->
