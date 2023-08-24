505dd8e3307ca08e459e2982e92f36376155550c
Gnanasekaran Loganathan <lgnanasekaran@gmail.com>
2023-08-24 16:14:51 -0400

    Created new pattern service-pattern

---
+++ patterns/services.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Title: Services
+ * Slug: twentytwentyfour/services
+ * Categories: featured
+ */
+?>
+
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"id":598,"sizeSlug":"full","linkDestination":"media"} -->
+<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/tt4_service.png" alt="" class="wp-image-598"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:heading {"level":3} -->
+<h3>Guiding your business through the project</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Experience the fusion of imagination and expertise with Études—the catalyst for architectural transformations that enrich the world around us.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="#">Learn More</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
