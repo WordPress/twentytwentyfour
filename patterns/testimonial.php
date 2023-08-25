c876b8fa456ed2445c8776ed48e61f5dc592ce46
KafleG <kafleg@git.wordpress.org>
2023-08-25 14:52:21 +0545

    Created testimonial pattern
    This pattern is created from home.php

---
+++ patterns/testimonial.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Title: Testimonial
+ * Slug: twentytwentyfour/testimonial
+ * Categories: text
+ */
+
+?>
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"12vh","bottom":"12vh","left":"6vw","right":"6vw"}},"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"backgroundColor":"contrast","textColor":"base","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-base-color has-contrast-background-color has-text-color has-background has-link-color" style="padding-top:12vh;padding-right:6vw;padding-bottom:12vh;padding-left:6vw"><!-- wp:group {"align":"wide","layout":{"type":"default"}} -->
+<div class="wp-block-group alignwide"><!-- wp:paragraph {"align":"center","style":{"typography":{"lineHeight":"1.2","letterSpacing":"-0.02em"}},"fontSize":"x-large","fontFamily":"cardo"} -->
+<p class="has-text-align-center has-cardo-font-family has-x-large-font-size" style="letter-spacing:-0.02em;line-height:1.2"><em>“Études has saved us thousands of hours of work and has unlocked insights we never thought possible.”</em></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"0.2rem"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+<div class="wp-block-group"><!-- wp:image {"id":86,"width":64,"aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none","style":{"border":{"radius":"100px"}},"className":"is-style-default"} -->
+<figure class="wp-block-image size-full is-resized has-custom-border is-style-default"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/tt4_testimonial_avatar.jpg" alt="" class="wp-image-86" style="border-radius:100px;aspect-ratio:1;object-fit:cover;width:64px" width="64"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontSize":"0.9rem"}}} -->
+<p style="font-size:0.9rem"><strong>Annie Steiner</strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|secondary"}}},"typography":{"fontSize":"0.9rem"}},"textColor":"secondary"} -->
+<p class="has-secondary-color has-text-color has-link-color" style="font-size:0.9rem">CEO, Greenprint</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
