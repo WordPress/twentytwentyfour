cf510768fa6c713477a7484ffd429c9e55afdf6d
KafleG <kafleg@git.wordpress.org>
2023-08-25 11:42:59 +0545

    Added Centered Statement Pattern

---
+++ patterns/centered-statement.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Title: Centered Statement
+ * Slug: twentytwentyfour/centered-statement
+ * Categories: text
+ */
+?>
+<!-- wp:group {"align":"wide","layout":{"type":"default"}} -->
+<div class="wp-block-group alignwide"><!-- wp:paragraph {"align":"center","style":{"typography":{"lineHeight":"1.3","fontStyle":"normal","fontWeight":"400"},"spacing":{"padding":{"top":"12rem","bottom":"12rem","right":"13rem","left":"13rem"}}},"backgroundColor":"base","textColor":"contrast","fontSize":"large","fontFamily":"cardo"} -->
+<p class="has-text-align-center has-contrast-color has-base-background-color has-text-color has-background has-cardo-font-family has-large-font-size" style="padding-top:12rem;padding-right:13rem;padding-bottom:12rem;padding-left:13rem;font-style:normal;font-weight:400;line-height:1.3">Queste fotografie non solo hanno catturato la bellezza fisica <br> del <em>Piva Arts &amp; Crafts Hotel</em>, ma hanno anche trasmesso le <br> emozioni e le sensazioni che l'hotel evoca nei suoi ospiti</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
