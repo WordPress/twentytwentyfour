0f72de04a7b0e272b394ea01564f80361495f80b
Mel Choyce-Dwan <melchoyce@users.noreply.github.com>
2023-08-26 14:45:43 -0400

    Update some spacing

--- patterns/posts.php
+++ patterns/posts.php
@@ -1,28 +1,19 @@
-<?php
-/**
- * Title: Posts
- * Slug: twentytwentyfour/posts
- * Categories: posts
- */
-
-?>
-
 <!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|80","bottom":"var:preset|spacing|80"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignfull" style="padding-top:var(--wp--preset--spacing--80);padding-bottom:var(--wp--preset--spacing--80)"><!-- wp:heading {"align":"wide"} -->
-<h2 class="wp-block-heading alignwide"><?php echo esc_html__( 'Watch, Read, Listen', 'twentytwentyfour' ); ?></h2>
+<div class="wp-block-group alignfull" style="padding-top:var(--wp--preset--spacing--80);padding-bottom:var(--wp--preset--spacing--80)"><!-- wp:heading {"align":"wide","style":{"spacing":{"margin":{"bottom":"var:preset|spacing|60"}}}} -->
+<h2 class="wp-block-heading alignwide" style="margin-bottom:var(--wp--preset--spacing--60)">Watch, Read, Listen</h2>
 <!-- /wp:heading -->
 
 <!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"left":"var:preset|spacing|60"}}}} -->
 <div class="wp-block-columns alignwide"><!-- wp:column {"width":"60%"} -->
 <div class="wp-block-column" style="flex-basis:60%"><!-- wp:query {"queryId":16,"query":{"perPage":1,"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false}} -->
 <div class="wp-block-query"><!-- wp:post-template -->
-<!-- wp:post-featured-image /-->
+<!-- wp:post-featured-image {"style":{"spacing":{"padding":{"bottom":"0"}}}} /-->
 
-<!-- wp:post-title {"style":{"typography":{"fontSize":"1.25rem"}}} /-->
+<!-- wp:post-title {"level":3,"style":{"typography":{"fontSize":"1.25rem"},"spacing":{"margin":{"top":"var:preset|spacing|60"}}}} /-->
 
 <!-- wp:post-excerpt {"fontSize":"small"} /-->
 
-<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|20"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
 <div class="wp-block-group"><!-- wp:post-date /-->
 
 <!-- wp:paragraph {"style":{"typography":{"fontSize":"0.8rem"}},"textColor":"secondary"} -->
@@ -42,7 +33,7 @@
 <div class="wp-block-query"><!-- wp:post-template {"style":{"spacing":{"blockGap":"var:preset|spacing|60"}}} -->
 <!-- wp:post-featured-image /-->
 
-<!-- wp:post-title {"style":{"typography":{"fontSize":"1.25rem","lineHeight":"1.3"}}} /-->
+<!-- wp:post-title {"level":3,"style":{"typography":{"fontSize":"1.25rem","lineHeight":"1.3"}}} /-->
 
 <!-- wp:post-excerpt {"fontSize":"small"} /-->
 
