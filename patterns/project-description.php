9b7a9dbf32d5642246520a4876f8cee415c4737a
Jordan <jordan@jordesign.com>
2023-08-30 16:30:32 +1000

    Update escapes to use esc_html_x

--- patterns/project-description.php
+++ patterns/project-description.php
@@ -11,19 +11,19 @@
 <div class="wp-block-group has-accent-2-background-color has-background" style="margin-top:0px;margin-bottom:0px;padding-top:6rem;padding-right:6rem;padding-bottom:6rem;padding-left:6rem"><!-- wp:columns {"align":"wide","style":{"spacing":{"margin":{"bottom":"6rem"}}}} -->
 <div class="wp-block-columns alignwide" style="margin-bottom:6rem"><!-- wp:column {"width":"50%"} -->
 <div class="wp-block-column" style="flex-basis:50%"><!-- wp:paragraph {"align":"left","fontSize":"small"} -->
-<p class="has-text-align-left has-small-font-size"><?php echo esc_html__( 'Piva Hotel - Overview', 'twentytwentyfour' ); ?></p>
+<p class="has-text-align-left has-small-font-size"><?php echo esc_attr_x( 'Piva Hotel - Overview', 'twentytwentyfour' ); ?></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:column -->
 
 <!-- wp:column {"width":"50%"} -->
 <div class="wp-block-column" style="flex-basis:50%"><!-- wp:paragraph {"style":{"layout":{"selfStretch":"fixed","flexSize":"50%"},"typography":{"fontSize":"2.38rem","lineHeight":"1.3","fontStyle":"normal","fontWeight":"400"}},"fontFamily":"cardo"} -->
-<p class="has-cardo-font-family" style="font-size:2.38rem;font-style:normal;font-weight:400;line-height:1.3"><?php echo esc_html__( 'Photo session for the Piva Arts &amp; Crafts Hotel near Lake Piva, Montenegro. An artificial lake was built in 1975 by dividing the Piva Kanyon to build a dam Mratinje.', 'twentytwentyfour' ); ?></p>
+<p class="has-cardo-font-family" style="font-size:2.38rem;font-style:normal;font-weight:400;line-height:1.3"><?php echo esc_attr_x( 'Photo session for the Piva Arts & Crafts Hotel near Lake Piva, Montenegro. An artificial lake was built in 1975 by dividing the Piva Kanyon to build a dam Mratinje.', 'twentytwentyfour' ); ?></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:column --></div>
 <!-- /wp:columns -->
 
 <!-- wp:image {"align":"wide","id":47365,"sizeSlug":"large","linkDestination":"none","style":{"layout":{"selfStretch":"fill","flexSize":null},"border":{"radius":"14px"}}} -->
-<figure class="wp-block-image alignwide size-large has-custom-border"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/tt4_project_description.jpg" alt="Photo of a kayak in the middle of a blue-green lake" class="wp-image-47365" style="border-radius:14px"/></figure>
+<figure class="wp-block-image alignwide size-large has-custom-border"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/tt4_project_description.jpg" alt="<?php echo esc_attr_x( 'Photo of a kayak in the middle of a blue-green lake" class="wp-image-47365" style="border-radius:14px"/></figure>
 <!-- /wp:image --></div>
 <!-- /wp:group -->
 
