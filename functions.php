9eec9f66342013cd544c2464077490585857c64a
esrat71 <39644617+esrat71@users.noreply.github.com>
2023-08-31 23:09:07 +0600

    have fixed translable string issue in pattern.
    have checked again , added all translable strings..

--- functions.php
+++ functions.php
@@ -1,57 +1,60 @@
 <?php
 /**
- * Twenty Twenty-Four functions and definitions
+ * Twenty Twenty Four functions and definitions
  *
  * @link https://developer.wordpress.org/themes/basics/theme-functions/
  *
- * @package Twenty Twenty-Four
- * @since Twenty Twenty-Four 1.0
+ * @package Twenty Twenty Four
+ * @since Twenty Twenty Four 1.0
  */
 
-if ( ! function_exists( 'twentytwentyfour_support' ) ):
 
-    /**
-     * Sets up theme defaults and registers support for various WordPress features.
-     *
-     * @since Twenty Twenty-Four 1.0
-     *
-     * @return void
-     */
-    function twentytwentyfour_support() {
-        // Enqueue editor styles.
-        add_editor_style( 'style.css') ;
-    }
+if ( ! function_exists( 'twentytwentyfour_support' ) ) :
+
+	/**
+	 * Sets up theme defaults and registers support for various WordPress features.
+	 *
+	 * @since Twenty Twenty Four 1.0
+	 *
+	 * @return void
+	 */
+	function twentytwentyfour_support() {
+		// Enqueue editor styles.
+		add_editor_style( 'style.css' );
+	}
 
 endif;
 
 add_action( 'after_setup_theme', 'twentytwentyfour_support' );
 
-if ( ! function_exists( 'twentytwentyfour_styles' ) ):
+if ( ! function_exists( 'twentytwentyfour_styles' ) ) :
 
-    /**
-     * Enqueue styles.
-     *
-     * @since Twenty Twenty-Four 1.0
-     *
-     * @return void
-     */
-    function twentytwentyfour_styles() {
+	/**
+	 * Enqueue styles.
+	 *
+	 * @since Twenty Twenty Four 1.0
+	 *
+	 * @return void
+	 */
+	function twentytwentyfour_styles() {
 
-        // Register theme stylesheet.
-        wp_register_style(
-            'twentytwentyfour-style',
-            get_template_directory_uri() . '/style.css',
-            [],
-            wp_get_theme()->get( 'Version' )
-        );
+		// Register theme stylesheet.
+		wp_register_style(
+			'twentytwentyfour-style',
+			get_template_directory_uri() . '/style.css',
+			array(),
+			wp_get_theme()->get( 'Version' )
+		);
 
-        // Enqueue theme stylesheet.
-        wp_enqueue_style( 'twentytwentyfour-style' );
-    }
+		// Enqueue theme stylesheet.
+		wp_enqueue_style( 'twentytwentyfour-style' );
+
+	}
 
 endif;
 
 add_action( 'wp_enqueue_scripts', 'twentytwentyfour_styles' );
+
 
 /**
  * Register block Styles
--- patterns/faq.php
+++ patterns/faq.php
@@ -1,19 +1,20 @@
 <?php
 /**
-* Title: faq
+* Title: Faq
 * Slug: twentytwentyfour/faq
 * Categories: about
 */
 ?>
 <!-- wp:group {"style":{"spacing":{"padding":{"top":"8vh","bottom":"8vh","left":"6px","right":"6px"}}},"backgroundColor":"contrast","layout":{"type":"constrained"}} -->
 <div class="wp-block-group has-contrast-background-color has-background" style="padding-top:8vh;padding-right:6px;padding-bottom:8vh;padding-left:6px"><!-- wp:heading {"style":{"elements":{"link":{"color":{"text":"var:preset|color|base-2"}}},"typography":{"fontSize":"10rem","letterSpacing":"-0.02em"}},"textColor":"base-2"} -->
-<h2 class="wp-block-heading has-base-2-color has-text-color has-link-color" style="font-size:10rem;letter-spacing:-0.02em"<?php echo esc_html_x( 'FAQs?', 'twentytwentyfour' ); ?>></h2>
+<h2 class="wp-block-heading has-base-2-color has-text-color has-link-color" style="font-size:10rem;letter-spacing:-0.02em"><?php echo esc_html_x( 'FAQs', 'twentytwentyfour'); ?></h2>
 <!-- /wp:heading -->
 
-<!-- wp:group {"layout":{"type":"default"}} -->
-<div class="wp-block-group"><!-- wp:list {"ordered":true,"style":{"elements":{"link":{"color":{"text":"var:preset|color|base-2"}}},"typography":{"lineHeight":"1.9"}},"textColor":"base-2","className":"is-style-faq-check-lists"} -->
-<ol class="is-style-faq-check-lists has-base-2-color has-text-color has-link-color" style="line-height:1.9"><!-- wp:list-item -->
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:list {"ordered":true,"type":"1","style":{"elements":{"link":{"color":{"text":"var:preset|color|base-2"}}}},"textColor":"base-2","className":"is-style-faq-checkmark-list is-style-faq-check-list is-style-faq-check-lists","fontSize":"medium"} -->
+<ol type="1" class="is-style-faq-checkmark-list is-style-faq-check-list is-style-faq-check-lists has-base-2-color has-text-color has-link-color has-medium-font-size"><!-- wp:list-item -->
 <li><?php echo esc_html_x( 'What is your process working in smaller projects?', 'twentytwentyfour' ); ?></li>
+
 <!-- /wp:list-item -->
 
 <!-- wp:list-item -->
@@ -21,7 +22,7 @@
 <!-- /wp:list-item -->
 
 <!-- wp:list-item -->
-<li><?php echo esc_html_x( 'I’d like to get to meet fellow architects, how can I do that?', 'twentytwentyfour' ); ?></li>
+<li><?php echo esc_html_x( 'I\'d like to get to meet fellow architects, how can I do that?', 'twentytwentyfour' ); ?></li>
 <!-- /wp:list-item -->
 
 <!-- wp:list-item -->
