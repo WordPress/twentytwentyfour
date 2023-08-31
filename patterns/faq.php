c5e7d6be024372458717d39855af9f4ba28a1c24
esrat71 <39644617+esrat71@users.noreply.github.com>
2023-08-31 18:01:07 +0600

    added Translation and custom list style changed
    Have translable all the hard coded string text and also changed the css file for list item

--- patterns/faq.php
+++ patterns/faq.php
@@ -6,27 +6,28 @@
 */
 ?>
 <!-- wp:group {"style":{"spacing":{"padding":{"top":"8vh","bottom":"8vh","left":"6px","right":"6px"}}},"backgroundColor":"contrast","layout":{"type":"constrained"}} -->
-    <div class="wp-block-group has-contrast-background-color has-background" style="padding-top:8vh;padding-right:6px;padding-bottom:8vh;padding-left:6px"><!-- wp:heading {"style":{"elements":{"link":{"color":{"text":"var:preset|color|base-2"}}},"typography":{"fontSize":"10rem","letterSpacing":"-0.02em"}},"textColor":"base-2"} -->
-    <h2 class="wp-block-heading has-base-2-color has-text-color has-link-color" style="font-size:10rem;letter-spacing:-0.02em">FAQs</h2>
+<div class="wp-block-group has-contrast-background-color has-background" style="padding-top:8vh;padding-right:6px;padding-bottom:8vh;padding-left:6px"><!-- wp:heading {"style":{"elements":{"link":{"color":{"text":"var:preset|color|base-2"}}},"typography":{"fontSize":"10rem","letterSpacing":"-0.02em"}},"textColor":"base-2"} -->
+<h2 class="wp-block-heading has-base-2-color has-text-color has-link-color" style="font-size:10rem;letter-spacing:-0.02em"<?php echo esc_html__( 'FAQs?', 'twentytwentyfour' ); ?>></h2>
 <!-- /wp:heading -->
 
-<!-- wp:group {"layout":{"type":"constrained"}} -->
-    <div class="wp-block-group"><!-- wp:list {"ordered":true,"type":"1","style":{"elements":{"link":{"color":{"text":"var:preset|color|base-2"}}}},"textColor":"base-2","className":"is-style-faq-checkmark-list is-style-faq-check-list is-style-faq-check-lists","fontSize":"medium"} -->
-    <ol type="1" class="is-style-faq-checkmark-list is-style-faq-check-list is-style-faq-check-lists has-base-2-color has-text-color has-link-color has-medium-font-size"><!-- wp:list-item -->
-    <li>↓ What is your process working in smaller projects?</li>
+<!-- wp:group {"layout":{"type":"default"}} -->
+<div class="wp-block-group"><!-- wp:list {"ordered":true,"style":{"elements":{"link":{"color":{"text":"var:preset|color|base-2"}}},"typography":{"lineHeight":"1.9"}},"textColor":"base-2","className":"is-style-faq-check-lists"} -->
+<ol class="is-style-faq-check-lists has-base-2-color has-text-color has-link-color" style="line-height:1.9"><!-- wp:list-item -->
+<li><?php echo esc_html__( 'What is your process working in smaller projects?', 'twentytwentyfour' ); ?></li>
 <!-- /wp:list-item -->
 
 <!-- wp:list-item -->
-    <li>↓ Who is behind Études?</li>
+<li><?php echo esc_html__( 'Who is behind Études?', 'twentytwentyfour' ); ?></li>
 <!-- /wp:list-item -->
 
 <!-- wp:list-item -->
-    <li>↓ I’d like to get to meet fellow architects, how can I do that?</li>
+<li><?php echo esc_html__( 'I’d like to get to meet fellow architects, how can I do that?', 'twentytwentyfour' ); ?></li>
 <!-- /wp:list-item -->
 
 <!-- wp:list-item -->
-    <li> ↓ Can I apply to be a part of the team or work as a contractor?</li>
+<li><?php echo esc_html__( 'Can I apply to be a part of the team or work as a contractor?', 'twentytwentyfour' ); ?></li>
 <!-- /wp:list-item --></ol>
 <!-- /wp:list --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
+
