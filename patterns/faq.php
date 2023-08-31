66c1ec8370a2a7b9c8c54f5fb08c0ca97081d2ec
esrat71 <39644617+esrat71@users.noreply.github.com>
2023-08-31 14:38:23 +0600

    added custom block style
    Added custom block style in function , custom css and the main faq file with list block

--- patterns/faq.php
+++ patterns/faq.php
@@ -1,44 +1,32 @@
 <?php
 /**
- * Title: Faq
- * slug: twentytwentyfour/faq
- * Categories: faq
+ * Title: faq
+ * Slug: twentytwentyfour/faq
+ * Categories: about
  */
-
 ?>
-
-<!-- wp:group {"style":{"color":{"background":"#101010"},"spacing":{"padding":{"top":"8vh","bottom":"8vh","left":"6px","right":"6px"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group has-background" style="background-color:#101010;padding-top:8vh;padding-right:6px;padding-bottom:8vh;padding-left:6px"><!-- wp:heading {"style":{"elements":{"link":{"color":{"text":"var:preset|color|base-2"}}},"typography":{"fontSize":"10rem","letterSpacing":"-0.02em"}},"textColor":"base-2"} -->
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"8vh","bottom":"8vh","left":"6px","right":"6px"}}},"backgroundColor":"contrast","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-contrast-background-color has-background" style="padding-top:8vh;padding-right:6px;padding-bottom:8vh;padding-left:6px"><!-- wp:heading {"style":{"elements":{"link":{"color":{"text":"var:preset|color|base-2"}}},"typography":{"fontSize":"10rem","letterSpacing":"-0.02em"}},"textColor":"base-2"} -->
 <h2 class="wp-block-heading has-base-2-color has-text-color has-link-color" style="font-size:10rem;letter-spacing:-0.02em">FAQs</h2>
 <!-- /wp:heading -->
 
-<!-- wp:group {"style":{"border":{"top":{"color":"#ffffff33","width":"1px"},"right":{"width":"0px","style":"none"},"bottom":{"color":"#ffffff33","width":"1px"},"left":{"width":"0px","style":"none"}},"spacing":{"padding":{"top":"15px","bottom":"15px"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group" style="border-top-color:#ffffff33;border-top-width:1px;border-right-style:none;border-right-width:0px;border-bottom-color:#ffffff33;border-bottom-width:1px;border-left-style:none;border-left-width:0px;padding-top:15px;padding-bottom:15px"><!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|base-2"}}},"layout":{"selfStretch":"fit","flexSize":null}},"textColor":"base-2","fontSize":"medium"} -->
-<p class="has-base-2-color has-text-color has-link-color has-medium-font-size">↓ What is your process working in smaller projects?</p>
-<!-- /wp:paragraph -->
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:list {"ordered":true,"type":"1","style":{"elements":{"link":{"color":{"text":"var:preset|color|base-2"}}}},"textColor":"base-2","className":"is-style-faq-checkmark-list is-style-faq-check-list is-style-faq-check-lists","fontSize":"medium"} -->
+<ol type="1" class="is-style-faq-checkmark-list is-style-faq-check-list is-style-faq-check-lists has-base-2-color has-text-color has-link-color has-medium-font-size"><!-- wp:list-item -->
+<li>↓ What is your process working in smaller projects?</li>
+<!-- /wp:list-item -->
 
-<!-- wp:separator {"style":{"color":{"background":"#ffffff33"}},"className":"is-style-wide"} -->
-<hr class="wp-block-separator has-text-color has-alpha-channel-opacity has-background is-style-wide" style="background-color:#ffffff33;color:#ffffff33"/>
-<!-- /wp:separator -->
+<!-- wp:list-item -->
+<li>↓ Who is behind Études?</li>
+<!-- /wp:list-item -->
 
-<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|base-2"}}}},"textColor":"base-2","fontSize":"medium"} -->
-<p class="has-base-2-color has-text-color has-link-color has-medium-font-size">↓ Who is behind Études?</p>
-<!-- /wp:paragraph -->
+<!-- wp:list-item -->
+<li>↓ I’d like to get to meet fellow architects, how can I do that?</li>
+<!-- /wp:list-item -->
 
-<!-- wp:separator {"style":{"color":{"background":"#ffffff33"}},"className":"is-style-wide"} -->
-<hr class="wp-block-separator has-text-color has-alpha-channel-opacity has-background is-style-wide" style="background-color:#ffffff33;color:#ffffff33"/>
-<!-- /wp:separator -->
-
-<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|base-2"}}}},"textColor":"base-2","fontSize":"medium"} -->
-<p class="has-base-2-color has-text-color has-link-color has-medium-font-size">↓ I’d like to get to meet fellow architects, how can I do that?</p>
-<!-- /wp:paragraph -->
-
-<!-- wp:separator {"style":{"color":{"background":"#ffffff33"}},"className":"is-style-wide"} -->
-<hr class="wp-block-separator has-text-color has-alpha-channel-opacity has-background is-style-wide" style="background-color:#ffffff33;color:#ffffff33"/>
-<!-- /wp:separator -->
-
-<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|base-2"}}}},"textColor":"base-2","fontSize":"medium"} -->
-<p class="has-base-2-color has-text-color has-link-color has-medium-font-size">↓ Can I apply to be a part of the team or work as a contractor?</p>
-<!-- /wp:paragraph --></div>
+<!-- wp:list-item -->
+<li> ↓ Can I apply to be a part of the team or work as a contractor?</li>
+<!-- /wp:list-item --></ol>
+<!-- /wp:list --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
