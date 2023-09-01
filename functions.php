16969083976044033e32131347c66f7735ef2ba7
esrat71 <39644617+esrat71@users.noreply.github.com>
2023-09-01 09:08:04 +0600

    tab indentions checked again

--- functions.php
+++ functions.php
@@ -1,12 +1,12 @@
 <?php
 /**
-* Twenty Twenty Four functions and definitions
-*
-* @link https://developer.wordpress.org/themes/basics/theme-functions/
-*
-* @package Twenty Twenty Four
-* @since Twenty Twenty Four 1.0
-*/
+ * Twenty Twenty Four functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package Twenty Twenty Four
+ * @since Twenty Twenty Four 1.0
+ */
 
 
 if ( ! function_exists( 'twentytwentyfour_support' ) ) :
@@ -14,11 +14,12 @@ if ( ! function_exists( 'twentytwentyfour_support' ) ) :
 	/**
 	 * Sets up theme defaults and registers support for various WordPress features.
 	 *
+	 * @return void
 	 * @since Twenty Twenty Four 1.0
 	 *
-	 * @return void
 	 */
-	function twentytwentyfour_support() {
+	function twentytwentyfour_support()
+	{
 		// Enqueue editor styles.
 		add_editor_style( 'style.css' );
 	}
@@ -32,11 +33,12 @@ if ( ! function_exists( 'twentytwentyfour_styles' ) ) :
 	/**
 	 * Enqueue styles.
 	 *
+	 * @return void
 	 * @since Twenty Twenty Four 1.0
 	 *
-	 * @return void
 	 */
-	function twentytwentyfour_styles() {
+	function twentytwentyfour_styles()
+	{
 
 		// Register theme stylesheet.
 		wp_register_style(
@@ -56,26 +58,27 @@ endif;
 add_action( 'wp_enqueue_scripts', 'twentytwentyfour_styles' );
 
 /**
-* Register block Styles
-*/
+ * Register block Styles
+ */
 
 if ( ! function_exists( 'twentytwentyfour_block_styles' ) ):
 	/**
-	* Register custom block styles
-	*
-	* @since Twenty Twenty-Four 1.0
-	*
-	* @return void
-	*/
-    function twentytwentyfour_block_styles() {
-        register_block_style(
-            'core/list',
-            [
-                'name'  => 'faq-check-lists',
-                'label' => __( 'Faq Checklist item', 'twentytwentyfour' ),
-            ]
-        );
-    }
+	 * Register custom block styles
+	 *
+	 * @return void
+	 * @since Twenty Twenty-Four 1.0
+	 *
+	 */
+	function twentytwentyfour_block_styles()
+	{
+		register_block_style(
+			'core/list',
+			[
+				'name' => 'faq-check-lists',
+				'label' => __( 'Faq Checklist item', 'twentytwentyfour' ),
+			]
+		);
+	}
 endif;
 
-add_action( 'init', 'twentytwentyfour_block_styles' );
+add_action( 'init', 'twentytwentyfour_block_styles' );add_action( 'init', 'twentytwentyfour_block_styles' );
