d7fb26fb970441c9681a3229c80516372ff631dc
devmuhib009 <142894870+devmuhib009@users.noreply.github.com>
2023-09-02 20:08:19 +0600

    Team Pattern PHP Validation
    Updated PHP Validation

--- patterns/team.php
+++ patterns/team.php
@@ -7,29 +7,29 @@
 ?>
 
 <!-- wp:heading {"textAlign":"center","fontSize":"large"} -->
-<h2 class="wp-block-heading has-text-align-center has-large-font-size"><?php echo esc_html_x( 'Meet our team', 'Header of Team Pattern','twentytwentyfour' ); ?></h2>
+<h2 class="wp-block-heading has-text-align-center has-large-font-size"><?php echo esc_html_x( 'Meet our team', 'Header of Team Pattern', 'twentytwentyfour' ); ?></h2>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph {"align":"center","fontSize":"small"} -->
-<p class="has-text-align-center has-small-font-size"><?php echo esc_html_x( 'Our comprehensive suite of professionals caters to a diverse team, <br>ranging from seasoned architects to renowned engineers.','Subheader of Team Pattern','twentytwentyfour' ); ?></p>
+<p class="has-text-align-center has-small-font-size"><?php echo esc_html_x( 'Our comprehensive suite of professionals caters to a diverse team, <br>ranging from seasoned architects to renowned engineers.', 'Subheader of Team Pattern', 'twentytwentyfour' ); ?></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:group {"style":{"spacing":{"blockGap":"3em"}},"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"center"}} -->
 <div class="wp-block-group">
 
 <!-- wp:image {"align":"center","id":19,"aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none","className":"is-style-rounded"} -->
-<figure class="wp-block-image aligncenter size-full is-style-rounded"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/team-member-1.jpg'); ?>" alt="" class="wp-image-19" style="aspect-ratio:1;object-fit:cover"/><figcaption class="wp-element-caption"><strong><?php echo esc_html_x( 'Francesca Piovani', 'Team Member Name','twentytwentyfour' ); ?></strong><br><?php echo esc_html_x( 'Founder, CEO & Architect', 'Team Member Designation','twentytwentyfour' ); ?></figcaption></figure>
+<figure class="wp-block-image aligncenter size-full is-style-rounded"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/team-member-1.jpg'); ?>" alt="" class="wp-image-19" style="aspect-ratio:1;object-fit:cover"/><figcaption class="wp-element-caption"><strong><?php echo esc_html_x( 'Francesca Piovani', 'Team Member Name', 'twentytwentyfour' ); ?></strong><br><?php echo esc_html_x( 'Founder, CEO & Architect', 'Team Member Designation', 'twentytwentyfour' ); ?></figcaption></figure>
 <!-- /wp:image -->
 
 <!-- wp:image {"align":"center","id":20,"aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none","className":"is-style-rounded"} -->
-<figure class="wp-block-image aligncenter size-full is-style-rounded"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/team-member-2.jpg'); ?>" alt="" class="wp-image-20" style="aspect-ratio:1;object-fit:cover"/><figcaption class="wp-element-caption"><strong><?php echo esc_html_x( 'Rhye Moore', 'Team Member Name','twentytwentyfour' ); ?></strong><br><?php echo esc_html_x( 'Engineering Manager', 'Team Member Designation','twentytwentyfour' ); ?></figcaption></figure>
+<figure class="wp-block-image aligncenter size-full is-style-rounded"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/team-member-2.jpg'); ?>" alt="" class="wp-image-20" style="aspect-ratio:1;object-fit:cover"/><figcaption class="wp-element-caption"><strong><?php echo esc_html_x( 'Rhye Moore', 'Team Member Name', 'twentytwentyfour' ); ?></strong><br><?php echo esc_html_x( 'Engineering Manager', 'Team Member Designation', 'twentytwentyfour' ); ?></figcaption></figure>
 <!-- /wp:image -->
 
 <!-- wp:image {"align":"center","id":22,"aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none","className":"is-style-rounded"} -->
-<figure class="wp-block-image aligncenter size-full is-style-rounded"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/team-member-3.jpg'); ?>" alt="" class="wp-image-22" style="aspect-ratio:1;object-fit:cover"/><figcaption class="wp-element-caption"><strong><?php echo esc_html_x( 'Helga Steiner', 'Team Member Name','twentytwentyfour' ); ?></strong><br><?php echo esc_html_x( 'Architect', 'Team Member Designation','twentytwentyfour' ); ?></figcaption></figure>
+<figure class="wp-block-image aligncenter size-full is-style-rounded"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/team-member-3.jpg'); ?>" alt="" class="wp-image-22" style="aspect-ratio:1;object-fit:cover"/><figcaption class="wp-element-caption"><strong><?php echo esc_html_x( 'Helga Steiner', 'Team Member Name', 'twentytwentyfour' ); ?></strong><br><?php echo esc_html_x( 'Architect', 'Team Member Designation', 'twentytwentyfour' ); ?></figcaption></figure>
 <!-- /wp:image -->
 
 <!-- wp:image {"align":"center","id":21,"aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"custom","className":"is-style-rounded"} -->
-<figure class="wp-block-image aligncenter size-full is-style-rounded"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/team-member-4.jpg'); ?>" alt="" class="wp-image-21" style="aspect-ratio:1;object-fit:cover"/><figcaption class="wp-element-caption"><strong><?php echo esc_html_x( 'Ivan Lawrence', 'Team Member Name','twentytwentyfour' ); ?></strong><br><?php echo esc_html_x( 'Project Manager', 'Team Member Designation','twentytwentyfour' ); ?></figcaption></figure>
+<figure class="wp-block-image aligncenter size-full is-style-rounded"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/team-member-4.jpg'); ?>" alt="" class="wp-image-21" style="aspect-ratio:1;object-fit:cover"/><figcaption class="wp-element-caption"><strong><?php echo esc_html_x( 'Ivan Lawrence', 'Team Member Name', 'twentytwentyfour' ); ?></strong><br><?php echo esc_html_x( 'Project Manager', 'Team Member Designation', 'twentytwentyfour' ); ?></figcaption></figure>
 <!-- /wp:image --></div>
 <!-- /wp:group -->
