17fc9c092542a8594f69ecc9318740f2b0669398
Anton Plauche <antonplauche92@gmail.com>
2023-09-04 08:21:52 -0500

    updates padding and margin to rems, adds side padding, and uses base color

--- patterns/testimonial.php
+++ patterns/testimonial.php
@@ -7,9 +7,9 @@
  */
 ?>
 
-<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"140px","bottom":"140px"}}},"backgroundColor":"contrast","textColor":"tertiary","layout":{"type":"default"}} -->
-<div class="wp-block-group alignwide has-tertiary-color has-contrast-background-color has-text-color has-background" style="padding-top:140px;padding-bottom:140px"><!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"bottom":"32px"}},"typography":{"lineHeight":"1.2"}},"textColor":"tertiary","fontSize":"x-large","fontFamily":"cardo"} -->
-  <p class="has-text-align-center has-tertiary-color has-text-color has-cardo-font-family has-x-large-font-size" style="margin-bottom:32px;line-height:1.2"><em><?php echo esc_html__('“Études has saved us thousands of hours of work and has unlocked insights we never thought possible.”', 'twentytwentyfour'); ?></em></p>
+<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"8.75rem","bottom":"8.75rem","left":"5.625rem","right":"5.625rem"}}},"backgroundColor":"contrast","textColor":"base","layout":{"type":"default"}} -->
+<div class="wp-block-group alignwide has-base-color has-contrast-background-color has-text-color has-background" style="padding-top:8.75rem;padding-right:5.625rem;padding-bottom:8.75rem;padding-left:5.625rem"><!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"bottom":"2rem"}},"typography":{"lineHeight":"1.2"}},"textColor":"base","fontSize":"x-large","fontFamily":"cardo"} -->
+  <p class="has-text-align-center has-base-color has-text-color has-cardo-font-family has-x-large-font-size" style="margin-bottom:2rem;line-height:1.2"><em><?php echo esc_html__('“Études has saved us thousands of hours of work and has unlocked insights we never thought possible.”', 'twentytwentyfour'); ?></em></p>
   <!-- /wp:paragraph -->
 
   <!-- wp:image {"align":"center","width":64,"height":64,"scale":"cover","sizeSlug":"thumbnail","linkDestination":"none","style":{"border":{"radius":"999px"}},"className":"is-style-rounded"} -->
@@ -20,8 +20,8 @@
   <p class="has-text-align-center has-inter-font-family"><strong><?php echo esc_html__('Annie Steiner', 'twentytwentyfour'); ?></strong></p>
   <!-- /wp:paragraph -->
 
-  <!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"top":"4px"}},"typography":{"fontStyle":"normal","fontWeight":"300"}},"fontFamily":"inter"} -->
-  <p class="has-text-align-center has-inter-font-family" style="margin-top:4px;font-style:normal;font-weight:300"><?php echo esc_html__('CEO, Greenprint', 'twentytwentyfour'); ?></p>
+  <!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"top":"0.25rem"}},"typography":{"fontStyle":"normal","fontWeight":"300"}},"fontFamily":"inter"} -->
+  <p class="has-text-align-center has-inter-font-family" style="margin-top:0.25rem;font-style:normal;font-weight:300"><?php echo esc_html__('CEO, Greenprint', 'twentytwentyfour'); ?></p>
   <!-- /wp:paragraph -->
 </div>
 <!-- /wp:group -->
