123dc9920f8fd7a81ef6731a98230436fd72a2ac
MaggieCabrera <maggie.cabrera@automattic.com>
2023-09-04 10:42:42 +0200

    aspect ratio and pattenr meta

--- patterns/posts.php
+++ patterns/posts.php
@@ -1,3 +1,11 @@
+<?php
+/**
+ * Title: Posts
+ * Slug: twentytwentyfour/posts
+ * Categories: query
+ * Block Types: core/query
+ */
+?>
 <!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|80","bottom":"var:preset|spacing|80"}}},"layout":{"type":"constrained"}} -->
 <div class="wp-block-group alignfull" style="padding-top:var(--wp--preset--spacing--80);padding-bottom:var(--wp--preset--spacing--80)"><!-- wp:heading {"align":"wide","style":{"spacing":{"margin":{"bottom":"var:preset|spacing|60"}}}} -->
 <h2 class="wp-block-heading alignwide" style="margin-bottom:var(--wp--preset--spacing--60)">Watch, Read, Listen</h2>
@@ -7,7 +15,7 @@
 <div class="wp-block-columns alignwide"><!-- wp:column {"width":"60%"} -->
 <div class="wp-block-column" style="flex-basis:60%"><!-- wp:query {"queryId":16,"query":{"perPage":1,"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false}} -->
 <div class="wp-block-query"><!-- wp:post-template -->
-<!-- wp:post-featured-image {"style":{"spacing":{"padding":{"bottom":"0"}}}} /-->
+<!-- wp:post-featured-image {"aspectRatio":"123/100","style":{"spacing":{"padding":{"bottom":"0"}}}} /-->
 
 <!-- wp:post-title {"level":3,"style":{"typography":{"fontSize":"1.25rem"},"spacing":{"margin":{"top":"var:preset|spacing|60"}}}} /-->
 
