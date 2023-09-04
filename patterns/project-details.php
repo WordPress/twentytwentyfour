3b142fc4963573309bd040d67343d556ab979229
JC <jc@webdevstudios.com>
2023-09-05 01:45:00 +0800

    #124 - Update pattern based on provided markup

--- patterns/project-details.php
+++ patterns/project-details.php
@@ -6,28 +6,36 @@
  */
 
 ?>
-<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40","left":"var:preset|spacing|40","right":"var:preset|spacing|40"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignfull" style="padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--40)"><!-- wp:columns {"verticalAlignment":"top","align":"wide"} -->
-<div class="wp-block-columns alignwide are-vertically-aligned-top"><!-- wp:column {"verticalAlignment":"top","width":"30%","style":{"spacing":{"padding":{"right":"15%"}}}} -->
-<div class="wp-block-column is-vertically-aligned-top" style="padding-right:15%;flex-basis:30%"><!-- wp:paragraph {"style":{"typography":{"letterSpacing":"-0.16px"}},"fontSize":"small"} -->
-<p class="has-small-font-size" style="letter-spacing:-0.16px"><?php echo esc_html__( 'Il mio obiettivo ha svelato l\'anima dell\'hotel, catturando gli intricati dettagli artigianali dei suoi arredi.', 'twentytwentyfour' ); ?></p>
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"8vh","bottom":"8vh","right":"6vw","left":"6vw"}}},"backgroundColor":"base","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull has-base-background-color has-background" style="padding-top:8vh;padding-right:6vw;padding-bottom:8vh;padding-left:6vw"><!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"top":"2em","left":"2em"}}}} -->
+<div class="wp-block-columns alignwide"><!-- wp:column {"width":"15%"} -->
+<div class="wp-block-column" style="flex-basis:15%"><!-- wp:paragraph {"style":{"typography":{"fontSize":"0.8rem","lineHeight":"1.4"}}} -->
+<p style="font-size:0.8rem;line-height:1.4"><?php echo esc_html__( 'The revitalized Art Gallery is set to redefine the cultural landscape of Toronto.', 'twentytwentyfour' ); ?></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:column -->
 
-<!-- wp:column {"verticalAlignment":"top","width":"","style":{"spacing":{"blockGap":"var:preset|spacing|30"}}} -->
-<div class="wp-block-column is-vertically-aligned-top"><!-- wp:heading {"fontSize":"x-large"} -->
-<h2 class="wp-block-heading has-x-large-font-size"><?php echo esc_html__( 'Una struttura boutique dall\'atmosfera artistica, un rifugio che invita i viaggiatori alla ricerca di un\'esperienza autentica e coinvolgente.', 'twentytwentyfour' ); ?></h2>
+<!-- wp:column {"width":"25%"} -->
+<div class="wp-block-column" style="flex-basis:25%"></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"60%","style":{"spacing":{"blockGap":"var:preset|spacing|30"}}} -->
+<div class="wp-block-column" style="flex-basis:60%"><!-- wp:heading -->
+<h2 class="wp-block-heading"><?php echo esc_html__( 'With meticulous attention to detail and a commitment to excellence, we create spaces that inspire, elevate, and enrich the lives of those who inhabit them.', 'twentytwentyfour' ); ?></h2>
 <!-- /wp:heading -->
 
-<!-- wp:group {"style":{"spacing":{"padding":{"top":"0","bottom":"0","right":"0"},"blockGap":"var:preset|spacing|30"}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between","verticalAlignment":"top"}} -->
-<div class="wp-block-group" style="padding-top:0;padding-right:0;padding-bottom:0"><!-- wp:paragraph {"style":{"layout":{"selfStretch":"fit","flexSize":null},"typography":{"letterSpacing":"-0.16px"}},"fontSize":"small"} -->
-<p class="has-small-font-size" style="letter-spacing:-0.16px"><?php echo esc_html__( 'La mia missione era tradurre la miscela unica di arte, artigianato e bellezza naturale dell\'hotel in una raccolta di fotografie evocative che risuonassero con gli spettatori e trasmettessero lo spirito di questa incantevole destinazione. Mi sono avvicinato all\'incarico con l\'obiettivo di catturare il carattere dell\'hotel nella sua interezza.', 'twentytwentyfour' ); ?></p>
-<!-- /wp:paragraph -->
-
-<!-- wp:paragraph {"style":{"typography":{"letterSpacing":"-0.16px"}},"fontSize":"small"} -->
-<p class="has-small-font-size" style="letter-spacing:-0.16px"><?php echo esc_html__( 'Ogni fotografia è stata meticolosamente composta per armonizzarsi con l\'estetica unica dell\'hotel, intrecciando senza soluzione di continuità elementi di arte e splendore naturale. Prima di mettere piede sul posto, mi sono impegnato in una ricerca completa per comprendere le sfumature architettoniche, i motivi artistici e il contesto culturale dell\'hotel.', 'twentytwentyfour' ); ?></p>
+<!-- wp:columns {"style":{"spacing":{"blockGap":{"top":"0","left":"var:preset|spacing|30"}}}} -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:paragraph {"style":{"typography":{"lineHeight":"1.4"}}} -->
+<p style="line-height:1.4"><?php echo esc_html__( 'The revitalized Art Gallery is set to redefine the cultural landscape of Toronto, serving as a nexus of artistic expression, community engagement, and architectural marvel. The expansion and renovation project pay homage to the Art Gallery’s rich history while embracing the future, ensuring that the gallery remains a beacon of inspiration for generations to come.', 'twentytwentyfour' ); ?></p>
 <!-- /wp:paragraph --></div>
-<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:paragraph {"style":{"typography":{"lineHeight":"1.4"}}} -->
+<p style="line-height:1.4"><?php echo esc_html__( 'As the Art Gallery embarks on this transformative journey, the expansion and renovation project stands as a testament to the enduring power of art, architecture, and the profound impact they have on the cultural fabric of a city. Green spaces, energy-efficient lighting, and a focus on natural ventilation contribute to the gallery\'s reduced carbon footprint.', 'twentytwentyfour' ); ?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
 <!-- /wp:column --></div>
 <!-- /wp:columns --></div>
 <!-- /wp:group -->
