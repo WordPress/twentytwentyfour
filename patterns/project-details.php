1a090b29841e5070c2cc0b6fdad5c8366bd6d6a6
JC <jc@webdevstudios.com>
2023-09-04 23:07:28 +0800

    #124 - Add Project Details Block Pattern

---
+++ patterns/project-details.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Title: Project Details
+ * Slug: twentytwentyfour/project-details
+ * Categories: text
+ */
+
+?>
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40","left":"var:preset|spacing|40","right":"var:preset|spacing|40"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull" style="padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--40)"><!-- wp:columns {"verticalAlignment":"top","align":"wide"} -->
+<div class="wp-block-columns alignwide are-vertically-aligned-top"><!-- wp:column {"verticalAlignment":"top","width":"30%","style":{"spacing":{"padding":{"right":"15%"}}}} -->
+<div class="wp-block-column is-vertically-aligned-top" style="padding-right:15%;flex-basis:30%"><!-- wp:paragraph {"style":{"typography":{"letterSpacing":"-0.16px"}},"fontSize":"small"} -->
+<p class="has-small-font-size" style="letter-spacing:-0.16px"><?php echo esc_html__( 'Il mio obiettivo ha svelato l\'anima dell\'hotel, catturando gli intricati dettagli artigianali dei suoi arredi.', 'twentytwentyfour' ); ?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"top","width":"","style":{"spacing":{"blockGap":"var:preset|spacing|30"}}} -->
+<div class="wp-block-column is-vertically-aligned-top"><!-- wp:heading {"fontSize":"x-large"} -->
+<h2 class="wp-block-heading has-x-large-font-size"><?php echo esc_html__( 'Una struttura boutique dall\'atmosfera artistica, un rifugio che invita i viaggiatori alla ricerca di un\'esperienza autentica e coinvolgente.', 'twentytwentyfour' ); ?></h2>
+<!-- /wp:heading -->
+
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"0","bottom":"0","right":"0"},"blockGap":"var:preset|spacing|30"}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between","verticalAlignment":"top"}} -->
+<div class="wp-block-group" style="padding-top:0;padding-right:0;padding-bottom:0"><!-- wp:paragraph {"style":{"layout":{"selfStretch":"fit","flexSize":null},"typography":{"letterSpacing":"-0.16px"}},"fontSize":"small"} -->
+<p class="has-small-font-size" style="letter-spacing:-0.16px"><?php echo esc_html__( 'La mia missione era tradurre la miscela unica di arte, artigianato e bellezza naturale dell\'hotel in una raccolta di fotografie evocative che risuonassero con gli spettatori e trasmettessero lo spirito di questa incantevole destinazione. Mi sono avvicinato all\'incarico con l\'obiettivo di catturare il carattere dell\'hotel nella sua interezza.', 'twentytwentyfour' ); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"typography":{"letterSpacing":"-0.16px"}},"fontSize":"small"} -->
+<p class="has-small-font-size" style="letter-spacing:-0.16px"><?php echo esc_html__( 'Ogni fotografia è stata meticolosamente composta per armonizzarsi con l\'estetica unica dell\'hotel, intrecciando senza soluzione di continuità elementi di arte e splendore naturale. Prima di mettere piede sul posto, mi sono impegnato in una ricerca completa per comprendere le sfumature architettoniche, i motivi artistici e il contesto culturale dell\'hotel.', 'twentytwentyfour' ); ?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
