56d23f87f4681fe18aea03c5e2902389f874c11c
Jessica Lyschik <luminuu@gmail.com>
2023-09-06 00:14:44 +0200

    Add translations

--- patterns/about-2.php
+++ patterns/about-2.php
@@ -13,7 +13,7 @@
 <div class="wp-block-columns alignwide"><!-- wp:column {"verticalAlignment":"stretch","width":"50%"} -->
 <div class="wp-block-column is-vertically-aligned-stretch" style="flex-basis:50%"><!-- wp:group {"style":{"dimensions":{"minHeight":"100%"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch","verticalAlignment":"space-between"}} -->
 <div class="wp-block-group" style="min-height:100%"><!-- wp:heading {"style":{"layout":{"selfStretch":"fixed","flexSize":"50%"}}} -->
-<h2 class="wp-block-heading">Études offers comprehensive consulting, management, design, and research solutions. Every architectural endeavor is an opportunity to shape the future.</h2>
+<h2 class="wp-block-heading"><?php esc_html_x('Études offers comprehensive consulting, management, design, and research solutions. Every architectural endeavor is an opportunity to shape the future.', 'Headline for the About pattern', 'twentytwentyfour'); ?></h2>
 <!-- /wp:heading -->
 
 <!-- wp:group {"layout":{"type":"constrained","contentSize":"300px","justifyContent":"left"}} -->
@@ -22,12 +22,12 @@
 <!-- /wp:spacer -->
 
 <!-- wp:paragraph {"style":{"layout":{"selfStretch":"fixed","flexSize":"50%"}}} -->
-<p>Leaving an indelible mark on the landscape of tomorrow.</p>
+<p><?php esc_html_x('Leaving an indelible mark on the landscape of tomorrow.', 'Description for the About pattern', 'twentytwentyfour'); ?></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:buttons -->
 <div class="wp-block-buttons"><!-- wp:button -->
-<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Learn More</a></div>
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button"><?php esc_html_x('Learn More', 'Call to Action button text', 'twentytwentyfour'); ?></a></div>
 <!-- /wp:button --></div>
 <!-- /wp:buttons --></div>
 <!-- /wp:group --></div>
