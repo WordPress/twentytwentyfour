1d1d20658df7f6212d82af5d5bf1487dee5be14b
Jordan <jordan@jordesign.com>
2023-09-05 10:12:31 +1000

    Changing border radius and removing default font weight

--- patterns/project-description.php
+++ patterns/project-description.php
@@ -14,13 +14,13 @@
 <!-- /wp:column -->
 
 <!-- wp:column {"width":"50%"} -->
-<div class="wp-block-column" style="flex-basis:50%"><!-- wp:paragraph {"style":{"layout":{"selfStretch":"fixed","flexSize":"50%"},"typography":{"fontSize":"2.38rem","lineHeight":"1.3","fontStyle":"normal","fontWeight":"400"}},"fontFamily":"cardo"} -->
-<p class="has-cardo-font-family" style="font-size:2.38rem;font-style:normal;font-weight:400;line-height:1.3"><?php echo esc_attr_x( 'Photo session for the Piva Arts & Crafts Hotel near Lake Piva, Montenegro. An artificial lake was built in 1975 by dividing the Piva Kanyon to build a dam Mratinje.', 'sample content for project description', 'twentytwentyfour' ); ?></p>
+<div class="wp-block-column" style="flex-basis:50%"><!-- wp:paragraph {"style":{"layout":{"selfStretch":"fixed","flexSize":"50%"},"typography":{"fontSize":"2.38rem","lineHeight":"1.3","fontStyle":"normal"}},"fontFamily":"cardo"} -->
+<p class="has-cardo-font-family" style="font-size:2.38rem;font-style:normal;line-height:1.3"><?php echo esc_attr_x( 'Photo session for the Piva Arts & Crafts Hotel near Lake Piva, Montenegro. An artificial lake was built in 1975 by dividing the Piva Kanyon to build a dam Mratinje.', 'sample content for project description', 'twentytwentyfour' ); ?></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:column --></div>
 <!-- /wp:columns -->
 
 <!-- wp:image {"align":"wide","id":47365,"sizeSlug":"large","linkDestination":"none","style":{"layout":{"selfStretch":"fill","flexSize":null},"border":{"radius":"14px"}}} -->
-<figure class="wp-block-image alignwide size-large has-custom-border"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/tt4_project_description.jpg" alt="<?php echo esc_attr_x( 'Photo of a kayak in the middle of a blue-green lake', 'alt text for sample image', 'twentytwentyfour' ); ?>" class="wp-image-47365" style="border-radius:14px"/></figure>
+<figure class="wp-block-image alignwide size-large has-custom-border"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/tt4_project_description.jpg" alt="<?php echo esc_attr_x( 'Photo of a kayak in the middle of a blue-green lake', 'alt text for sample image', 'twentytwentyfour' ); ?>" class="wp-image-47365" style="border-radius:16px"/></figure>
 <!-- /wp:image --></div>
 <!-- /wp:group -->
