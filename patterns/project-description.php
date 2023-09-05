f74bb3e67d3944826aa139c569a8104617c3f6cb
Jordan <jordan@jordesign.com>
2023-09-05 10:27:01 +1000

    Change Column widths to match original pattern

--- patterns/project-description.php
+++ patterns/project-description.php
@@ -7,14 +7,18 @@
 ?>
 <!-- wp:group {"style":{"spacing":{"padding":{"top":"6rem","right":"6rem","bottom":"6rem","left":"6rem"},"margin":{"top":"0px","bottom":"0px"}}},"backgroundColor":"accent-2","layout":{"type":"constrained"}} -->
 <div class="wp-block-group has-accent-2-background-color has-background" style="margin-top:0px;margin-bottom:0px;padding-top:6rem;padding-right:6rem;padding-bottom:6rem;padding-left:6rem"><!-- wp:columns {"align":"wide","style":{"spacing":{"margin":{"bottom":"6rem"}}}} -->
-<div class="wp-block-columns alignwide" style="margin-bottom:6rem"><!-- wp:column {"width":"50%"} -->
-<div class="wp-block-column" style="flex-basis:50%"><<!-- wp:heading {"textAlign":"left","fontSize":"small","fontFamily":"system-font"} -->
+<div class="wp-block-columns alignwide" style="margin-bottom:6rem"><!-- wp:column {"width":"40%"} -->
+<div class="wp-block-column" style="flex-basis:40%"><<!-- wp:heading {"textAlign":"left","fontSize":"small","fontFamily":"system-font"} -->
 <h2 class="wp-block-heading has-text-align-left has-system-font-font-family has-small-font-size"><?php echo esc_attr_x( 'Piva Hotel - Overview', 'Sample content for project title', 'twentytwentyfour' ); ?></h2>
 <!-- /wp:heading --></div>
 <!-- /wp:column -->
 
-<!-- wp:column {"width":"50%"} -->
-<div class="wp-block-column" style="flex-basis:50%"><!-- wp:paragraph {"style":{"layout":{"selfStretch":"fixed","flexSize":"50%"},"typography":{"fontSize":"2.38rem","lineHeight":"1.3","fontStyle":"normal"}},"fontFamily":"cardo"} -->
+<!-- wp:column {"width":"9%"} -->
+<div class="wp-block-column" style="flex-basis:9%"></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"60%"} -->
+<div class="wp-block-column" style="flex-basis:60%"><!-- wp:paragraph {"style":{"layout":{"selfStretch":"fixed","flexSize":"50%"},"typography":{"fontSize":"2.38rem","lineHeight":"1.3","fontStyle":"normal"}},"fontFamily":"cardo"} -->
 <p class="has-cardo-font-family" style="font-size:2.38rem;font-style:normal;line-height:1.3"><?php echo esc_attr_x( 'Photo session for the Piva Arts & Crafts Hotel near Lake Piva, Montenegro. An artificial lake was built in 1975 by dividing the Piva Kanyon to build a dam Mratinje.', 'sample content for project description', 'twentytwentyfour' ); ?></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:column --></div>
