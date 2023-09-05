c5c6094cdfa99ee6ae8f1d5448a699b9aa1672e1
MaggieCabrera <maggie.cabrera@automattic.com>
2023-09-05 22:20:58 +0200

    restored translations

--- patterns/services-cta.php
+++ patterns/services-cta.php
@@ -4,7 +4,6 @@
  * Slug: twentytwentyfour/services-cta
  * Categories: call-to-action
  */
-
 ?>
 
 <!-- wp:columns {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|50","right":"var:preset|spacing|50"},"blockGap":{"top":"var:preset|spacing|30","left":"var:preset|spacing|50"},"margin":{"top":"0","bottom":"0"}}},"backgroundColor":"contrast-3"} -->
@@ -16,16 +15,16 @@
 
 <!-- wp:column -->
 <div class="wp-block-column"><!-- wp:heading -->
-<h2 class="wp-block-heading">Guiding your business through the project</h2>
+<h2 class="wp-block-heading"><?php echo esc_html_x( 'Guiding your business through the project', 'Heading of The Service Call to Action', 'twentytwentyfour' ); ?></h2>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph -->
-<p>Experience the fusion of imagination and expertise with Études—the catalyst for architectural transformations that enrich the world around us.</p>
+<p><?php echo esc_html_x( 'Experience the fusion of imagination and expertise with Études—the catalyst for architectural transformations that enrich the world around us.', 'Service Description of Call to Action', 'twentytwentyfour' ); ?></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:buttons -->
 <div class="wp-block-buttons"><!-- wp:button -->
-<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Learn More</a></div>
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button"><?php echo esc_html_x( 'Learn More', 'Call to Action Button Text', 'twentytwentyfour' ); ?></a></div>
 <!-- /wp:button --></div>
 <!-- /wp:buttons --></div>
 <!-- /wp:column --></div>
