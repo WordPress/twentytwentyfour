be37d6148adcf156b122ae6380fc7c6dfbb7dc70
MaggieCabrera <maggie.cabrera@automattic.com>
2023-09-05 22:46:42 +0200

    added full page pattern for business about page

---
+++ patterns/business-about.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Title: Business About
+ * Slug: twentytwentyfour/business-about
+ * Categories: featured
+ * Block Types: core/post-content
+ */
+?>
+
+<!-- wp:pattern {"slug":"twentytwentyfour/about-2"} /-->
+<!-- wp:pattern {"slug":"twentytwentyfour/project-details"} /-->
+<!-- wp:pattern {"slug":"twentytwentyfour/services-cta"} /-->
+<!-- wp:pattern {"slug":"twentytwentyfour/team"} /-->
+<!-- wp:pattern {"slug":"twentytwentyfour/clients"} /-->
+<!-- wp:pattern {"slug":"twentytwentyfour/faq"} /-->
+<!-- wp:pattern {"slug":"twentytwentyfour/left-aligned-cta-image"} /-->
