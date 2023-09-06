3c4ea51168d1cfac2d6bf46110416f7a6a4d25f2
Huzaifa Al Mesbah <63150399+huzaifaalmesbah@users.noreply.github.com>
2023-09-06 19:50:27 +0600

    Update Alt Text

--- patterns/about-2.php
+++ patterns/about-2.php
@@ -36,7 +36,7 @@
 
 <!-- wp:column {"verticalAlignment":"center","width":"50%"} -->
 <div class="wp-block-column is-vertically-aligned-center" style="flex-basis:50%"><!-- wp:image {"aspectRatio":"3/4","scale":"cover","sizeSlug":"large","linkDestination":"none"} -->
-<figure class="wp-block-image size-large"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/tt4_about_1.jpg" alt="" style="aspect-ratio:3/4;object-fit:cover"/></figure>
+<figure class="wp-block-image size-large"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/tt4_about_1.jpg" alt="<?php echo esc_attr( 'Picture Of Business About', 'twentytwentyfour' ); ?>" style="aspect-ratio:3/4;object-fit:cover"/></figure>
 <!-- /wp:image --></div>
 <!-- /wp:column --></div>
 <!-- /wp:columns --></div>
