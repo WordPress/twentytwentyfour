c5d31a2e67ca669c776e5217fd04e10eb0b93514
Huzaifa Al Mesbah <huzaifah4m@gmail.com>
2023-09-06 14:20:00 +0600

    fix some issues
    Remove unnecessary padding  & class, add esc_html_x and esc_attr

--- patterns/features-with-images.php
+++ patterns/features-with-images.php
@@ -7,18 +7,18 @@
 
 ?>
 
-<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"6vh","bottom":"8vh"}}},"layout":{"type":"constrained","wideSize":"1280px"}} -->
+<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"6vh","bottom":"8vh"}}},"layout":{"type":"constrained"}} -->
 <div class="wp-block-group alignwide" style="padding-top:6vh;padding-bottom:8vh"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|40"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
-<div class="wp-block-group"><!-- wp:paragraph {"align":"center","style":{"layout":{"selfStretch":"fit","flexSize":null},"spacing":{"padding":{"right":"0","left":"0"}},"elements":{"link":{"color":{"text":"var:preset|color|custom-borders"}}}},"textColor":"custom-borders","fontSize":"large"} -->
-<p class="has-text-align-center has-custom-borders-color has-text-color has-link-color has-large-font-size" style="padding-right:0;padding-left:0"><?php echo esc_html__( '✳︎', 'twentytwentyfour' ); ?></p>
+<div class="wp-block-group"><!-- wp:paragraph {"align":"center","style":{"layout":{"selfStretch":"fit","flexSize":null},"elements":{"link":{"color":{"text":"var:preset|color|custom-borders"}}}},"textColor":"custom-borders","fontSize":"large"} -->
+<p class="has-text-align-center has-custom-borders-color has-text-color has-link-color has-large-font-size"><?php echo esc_html__( '✳︎', 'twentytwentyfour' ); ?></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:heading {"textAlign":"center"} -->
-<h2 class="wp-block-heading has-text-align-center"><?php echo esc_html__( 'An array of resources', 'twentytwentyfour' ); ?></h2>
+<h2 class="wp-block-heading has-text-align-center"><?php echo esc_html_x( 'An array of resources', 'Feature title', 'twentytwentyfour' ); ?></h2>
 <!-- /wp:heading -->
 
-<!-- wp:paragraph {"align":"center","style":{"layout":{"selfStretch":"fit","flexSize":null},"spacing":{"padding":{"right":"0","left":"0"}}},"fontSize":"small"} -->
-<p class="has-text-align-center has-small-font-size" style="padding-right:0;padding-left:0"><?php echo esc_html__( 'Our comprehensive suite of professional services caters to a diverse clientele, ranging from homeowners to commercial developers.', 'twentytwentyfour' ); ?></p>
+<!-- wp:paragraph {"align":"center","style":{"layout":{"selfStretch":"fit","flexSize":null},"fontSize":"small"} -->
+<p class="has-text-align-center has-small-font-size"><?php echo esc_html_x( 'Our comprehensive suite of professional services caters to a diverse clientele, ranging from homeowners to commercial developers.', 'Service description', 'twentytwentyfour' ); ?></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group -->
 
@@ -29,33 +29,33 @@
 <!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|50","left":"var:preset|spacing|80"}}}} -->
 <div class="wp-block-columns alignwide"><!-- wp:column {"verticalAlignment":"center"} -->
 <div class="wp-block-column is-vertically-aligned-center"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"layout":{"type":"flex","orientation":"vertical"}} -->
-<div class="wp-block-group"><!-- wp:paragraph {"align":"left","style":{"layout":{"selfStretch":"fit","flexSize":null},"spacing":{"padding":{"right":"0","left":"0"}},"elements":{"link":{"color":{"text":"var:preset|color|custom-borders"}}}},"textColor":"custom-borders","fontSize":"large"} -->
-<p class="has-text-align-left has-custom-borders-color has-text-color has-link-color has-large-font-size" style="padding-right:0;padding-left:0"><?php echo esc_html__( '✳︎', 'twentytwentyfour' ); ?></p>
+<div class="wp-block-group"><!-- wp:paragraph {"align":"left","style":{"layout":{"selfStretch":"fit","flexSize":null},"elements":{"link":{"color":{"text":"var:preset|color|custom-borders"}}}},"textColor":"custom-borders","fontSize":"large"} -->
+<p class="has-text-align-left has-custom-borders-color has-text-color has-link-color has-large-font-size"><?php echo esc_html__( '✳︎', 'twentytwentyfour' ); ?></p>
 <!-- /wp:paragraph -->
 
-<!-- wp:heading {"level":3,"fontSize":"large"} -->
-<h3 class="wp-block-heading has-large-font-size"><?php echo esc_html__( 'Études Architect App', 'twentytwentyfour' ); ?></h3>
+<!-- wp:heading {"level":3} -->
+<h3 class="wp-block-heading"><?php echo esc_html_x( 'Études Architect App', 'Feature title', 'twentytwentyfour' ); ?></h3>
 <!-- /wp:heading --></div>
 <!-- /wp:group -->
 
 <!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"layout":{"type":"flex","orientation":"vertical"}} -->
 <div class="wp-block-group"><!-- wp:paragraph -->
-<p><strong><?php echo esc_html__( '✓', 'twentytwentyfour' ); ?> </strong><?php echo esc_html__( 'Collaborate with fellow architects', 'twentytwentyfour' ); ?></p>
+<p><strong><?php echo esc_html__( '✓', 'twentytwentyfour' ); ?> </strong><?php echo esc_html_x( 'Collaborate with fellow architects', 'Feature description', 'twentytwentyfour' ); ?></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:paragraph -->
-<p><strong><?php echo esc_html__( '✓', 'twentytwentyfour' ); ?> </strong><?php echo esc_html__( 'Showcase your projects', 'twentytwentyfour' ); ?></p>
+<p><strong><?php echo esc_html__( '✓', 'twentytwentyfour' ); ?> </strong><?php echo esc_html_x( 'Showcase your projects', 'Feature description', 'twentytwentyfour' ); ?></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:paragraph -->
-<p><strong><?php echo esc_html__( '✓', 'twentytwentyfour' ); ?> </strong><?php echo esc_html__( 'Experience the world of architecture like never before', 'twentytwentyfour' ); ?></p>
+<p><strong><?php echo esc_html__( '✓', 'twentytwentyfour' ); ?> </strong><?php echo esc_html_x( 'Experience the world of architecture like never before', 'Feature description', 'twentytwentyfour' ); ?></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group --></div>
 <!-- /wp:column -->
 
 <!-- wp:column -->
-<div class="wp-block-column"><!-- wp:image {"id":370,"sizeSlug":"large","linkDestination":"none"} -->
-<figure class="wp-block-image size-large"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/tt4_business_image_app.jpg" alt="" class="wp-image-370"/></figure>
+<div class="wp-block-column"><!-- wp:image {"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/tt4_business_image_app.jpg" alt="<?php echo esc_attr( 'TT4 Business Image', 'twentytwentyfour' ); ?>" /></figure>
 <!-- /wp:image --></div>
 <!-- /wp:column --></div>
 <!-- /wp:columns -->
@@ -66,33 +66,33 @@
 
 <!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|50","left":"var:preset|spacing|80"}}}} -->
 <div class="wp-block-columns alignwide"><!-- wp:column -->
-<div class="wp-block-column"><!-- wp:image {"id":67,"sizeSlug":"large","linkDestination":"none"} -->
-<figure class="wp-block-image size-large"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/tt4_home_image-2.jpg" alt="" class="wp-image-67"/></figure>
+<div class="wp-block-column"><!-- wp:image {"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/tt4_home_image-2.jpg" alt="<?php echo esc_attr( 'Home Image 2', 'twentytwentyfour' ); ?>"/></figure>
 <!-- /wp:image --></div>
 <!-- /wp:column -->
 
 <!-- wp:column {"verticalAlignment":"center"} -->
 <div class="wp-block-column is-vertically-aligned-center"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"layout":{"type":"flex","orientation":"vertical"}} -->
-<div class="wp-block-group"><!-- wp:paragraph {"align":"left","style":{"layout":{"selfStretch":"fit","flexSize":null},"spacing":{"padding":{"right":"0","left":"0"}},"elements":{"link":{"color":{"text":"var:preset|color|custom-borders"}}}},"textColor":"custom-borders","fontSize":"large"} -->
-<p class="has-text-align-left has-custom-borders-color has-text-color has-link-color has-large-font-size" style="padding-right:0;padding-left:0"><?php echo esc_html__( '✳︎', 'twentytwentyfour' ); ?></p>
+<div class="wp-block-group"><!-- wp:paragraph {"align":"left","style":{"layout":{"selfStretch":"fit","flexSize":null},"elements":{"link":{"color":{"text":"var:preset|color|custom-borders"}}}},"textColor":"custom-borders","fontSize":"large"} -->
+<p class="has-text-align-left has-custom-borders-color has-text-color has-link-color has-large-font-size"><?php echo esc_html__( '✳︎', 'twentytwentyfour' ); ?></p>
 <!-- /wp:paragraph -->
 
-<!-- wp:heading {"level":3,"fontSize":"large"} -->
-<h3 class="wp-block-heading has-large-font-size"><?php echo esc_html__( 'Études Newsletter', 'twentytwentyfour' ); ?></h3>
+<!-- wp:heading {"level":3} -->
+<h3 class="wp-block-heading"><?php echo esc_html_x( 'Études Newsletter', 'Feature title', 'twentytwentyfour' ); ?></h3>
 <!-- /wp:heading --></div>
 <!-- /wp:group -->
 
 <!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"layout":{"type":"flex","orientation":"vertical"}} -->
 <div class="wp-block-group"><!-- wp:paragraph -->
-<p><strong><?php echo esc_html__( '✓', 'twentytwentyfour' ); ?> </strong><?php echo esc_html__( 'Dive into a world of thought-provoking articles', 'twentytwentyfour' ); ?></p>
+<p><strong><?php echo esc_html__( '✓', 'twentytwentyfour' ); ?> </strong><?php echo esc_html_x( 'Dive into a world of thought-provoking articles', 'Feature description', 'twentytwentyfour' ); ?></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:paragraph -->
-<p><strong><?php echo esc_html__( '✓', 'twentytwentyfour' ); ?> </strong><?php echo esc_html__( 'Read case studies that celebrate the artistry of architecture', 'twentytwentyfour' ); ?></p>
+<p><strong><?php echo esc_html__( '✓', 'twentytwentyfour' ); ?> </strong><?php echo esc_html_x( 'Read case studies that celebrate the artistry of architecture', 'Feature description', 'twentytwentyfour' ); ?></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:paragraph -->
-<p><strong><?php echo esc_html__( '✓', 'twentytwentyfour' ); ?> </strong><?php echo esc_html__( 'Gain exclusive access to design insights', 'twentytwentyfour' ); ?></p>
+<p><strong><?php echo esc_html__( '✓', 'twentytwentyfour' ); ?> </strong><?php echo esc_html_x( 'Gain exclusive access to design insights', 'Feature description', 'twentytwentyfour' ); ?></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group --></div>
 <!-- /wp:column --></div>
