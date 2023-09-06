a48660637b4313c71167cf878378dbb92de29867
Rich Tabor <hi@richtabor.com>
2023-09-06 14:39:44 -0400

    Update project-details.php

--- patterns/project-details.php
+++ patterns/project-details.php
@@ -2,37 +2,36 @@
 /**
  * Title: Project Details
  * Slug: twentytwentyfour/project-details
- * Categories: text
+ * Categories: text, featured
  */
 
 ?>
-<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40","left":"var:preset|spacing|20","right":"var:preset|spacing|20"}}},"backgroundColor":"base","layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignfull has-base-background-color has-background" style="padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--20);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--20)"><!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"top":"2em","left":"2em"}}}} -->
-<div class="wp-block-columns alignwide"><!-- wp:column {"width":"15%"} -->
-<div class="wp-block-column" style="flex-basis:15%"><!-- wp:paragraph {"style":{"typography":{"fontSize":"0.8rem","lineHeight":"1.4"}}} -->
-<p style="font-size:0.8rem;line-height:1.4"><?php echo esc_html_x( 'The revitalized Art Gallery is set to redefine the cultural landscape of Toronto.', 'A short description of project details.', 'twentytwentyfour' ); ?></p>
-<!-- /wp:paragraph --></div>
-<!-- /wp:column -->
 
-<!-- wp:column {"width":"25%"} -->
-<div class="wp-block-column" style="flex-basis:25%"></div>
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|50","right":"var:preset|spacing|50"},"margin":{"top":"0","bottom":"0"}}},"backgroundColor":"base","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull has-base-background-color has-background" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)"><!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"top":"2em","left":"2em"}}}} -->
+<div class="wp-block-columns alignwide"><!-- wp:column {"width":"40%"} -->
+<div class="wp-block-column" style="flex-basis:40%"><!-- wp:group {"layout":{"type":"constrained","contentSize":"260px","justifyContent":"left"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"fontSize":"small"} -->
+<p class="has-small-font-size">The revitalized art gallery is set to redefine cultural landscape.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
 <!-- /wp:column -->
 
 <!-- wp:column {"width":"60%","style":{"spacing":{"blockGap":"var:preset|spacing|30"}}} -->
 <div class="wp-block-column" style="flex-basis:60%"><!-- wp:heading -->
-<h2 class="wp-block-heading"><?php echo esc_html_x( 'With meticulous attention to detail and a commitment to excellence, we create spaces that inspire, elevate, and enrich the lives of those who inhabit them.', 'A long heading about project details.', 'twentytwentyfour' ); ?></h2>
+<h2 class="wp-block-heading">With meticulous attention to detail and a commitment to excellence, we create spaces that inspire, elevate, and enrich the lives of those who inhabit them.</h2>
 <!-- /wp:heading -->
 
 <!-- wp:columns {"style":{"spacing":{"blockGap":{"top":"var:preset|spacing|30","left":"var:preset|spacing|30"}}}} -->
 <div class="wp-block-columns"><!-- wp:column -->
-<div class="wp-block-column"><!-- wp:paragraph {"style":{"typography":{"lineHeight":"1.4"}}} -->
-<p style="line-height:1.4"><?php echo esc_html_x( 'The revitalized Art Gallery is set to redefine the cultural landscape of Toronto, serving as a nexus of artistic expression, community engagement, and architectural marvel. The expansion and renovation project pay homage to the Art Gallery’s rich history while embracing the future, ensuring that the gallery remains a beacon of inspiration for generations to come.', 'Project details.', 'twentytwentyfour' ); ?></p>
+<div class="wp-block-column"><!-- wp:paragraph {"style":{"layout":{"selfStretch":"fill","flexSize":null}}} -->
+<p>The revitalized Art Gallery is set to redefine the cultural landscape of Toronto, serving as a nexus of artistic expression, community engagement, and architectural marvel. The expansion and renovation project pay homage to the Art Gallery’s rich history while embracing the future, ensuring that the gallery remains a beacon of inspiration.</p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:column -->
 
 <!-- wp:column -->
-<div class="wp-block-column"><!-- wp:paragraph {"style":{"typography":{"lineHeight":"1.4"}}} -->
-<p style="line-height:1.4"><?php echo esc_html_x( 'As the Art Gallery embarks on this transformative journey, the expansion and renovation project stands as a testament to the enduring power of art, architecture, and the profound impact they have on the cultural fabric of a city. Green spaces, energy-efficient lighting, and a focus on natural ventilation contribute to the gallery\'s reduced carbon footprint.', 'Project details.', 'twentytwentyfour' ); ?></p>
+<div class="wp-block-column"><!-- wp:paragraph -->
+<p>The revitalized Art Gallery is set to redefine the cultural landscape of Toronto, serving as a nexus of artistic expression, community engagement, and architectural marvel. The expansion and renovation project pay homage to the Art Gallery’s rich history while embracing the future, ensuring that the gallery remains a beacon of inspiration.</p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:column --></div>
 <!-- /wp:columns --></div>
