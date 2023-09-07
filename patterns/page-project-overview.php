ad430c80fe2aa7a67ef79b237b62df1bc797a6f6
Maggie <maggie.cabrera@automattic.com>
2023-09-07 22:28:23 +0200

    Add project overview page pattern (#269)
    * added pattern
    
    * added fullscreen image pattern
    
    * linting
    
    * Rename pattern file with page- prefix
    
    * Set full screen image pattern to false
    
    * Use spacer instead of placeholder text
    
    ---------
    
    Co-authored-by: Rich Tabor <hi@richtabor.com>

---
+++ patterns/page-project-overview.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Title: Project Overview
+ * Slug: twentytwentyfour/page-project-overview
+ * Categories: about
+ * Keywords: page, starter
+ * Block Types: core/post-content
+ * Post Types: page
+ */
+?>
+
+<!-- wp:pattern {"slug":"twentytwentyfour/project-description"} /-->
+<!-- wp:pattern {"slug":"twentytwentyfour/project-details"} /-->
+<!-- wp:pattern {"slug":"twentytwentyfour/full-screen-image"} /-->
+<!-- wp:pattern {"slug":"twentytwentyfour/centered-statement"} /-->
+<!-- wp:pattern {"slug":"twentytwentyfour/project-layout"} /-->
