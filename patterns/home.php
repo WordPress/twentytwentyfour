9b51e20408cb684f6dbb336fce0da444e6f48652
MaggieCabrera <maggie.cabrera@automattic.com>
2023-09-07 18:09:52 +0200

    fixes alignment issues and removes wrapper from home pattern

--- patterns/home.php
+++ patterns/home.php
@@ -7,13 +7,10 @@
  * Template Types: home
  */
 ?>
-<!-- wp:group {"tagName":"main","style":{"spacing":{"blockGap":"0","margin":{"top":"0"}}},"layout":{"type":"default"}} -->
-<main class="wp-block-group" style="margin-top:0">
-	<!-- wp:pattern {"slug":"twentytwentyfour/hero"} /-->
-	<!-- wp:pattern {"slug":"twentytwentyfour/feature-grid"} /-->
-	<!-- wp:pattern {"slug":"twentytwentyfour/features-with-images"} /-->
-	<!-- wp:pattern {"slug":"twentytwentyfour/testimonial"} /-->
-	<!-- wp:pattern {"slug":"twentytwentyfour/posts"} /-->
-	<!-- wp:pattern {"slug":"twentytwentyfour/cta"} /-->
-</main>
-<!-- /wp:group -->
+
+<!-- wp:pattern {"slug":"twentytwentyfour/hero"} /-->
+<!-- wp:pattern {"slug":"twentytwentyfour/feature-grid"} /-->
+<!-- wp:pattern {"slug":"twentytwentyfour/features-with-images"} /-->
+<!-- wp:pattern {"slug":"twentytwentyfour/testimonial"} /-->
+<!-- wp:pattern {"slug":"twentytwentyfour/posts"} /-->
+<!-- wp:pattern {"slug":"twentytwentyfour/cta"} /-->
