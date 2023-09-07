1f22a9796538b6f7d423dca8e8d5734b757a590b
Juan Aldasoro <juanfraa@gmail.com>
2023-09-07 16:58:24 +0200

    Update clients pattern

--- patterns/clients.php
+++ patterns/clients.php
@@ -6,8 +6,8 @@
  */
 
 ?>
-<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|20","right":"var:preset|spacing|20"}},"border":{"radius":"16px"}},"backgroundColor":"base-2","layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignwide has-base-2-background-color has-background" style="border-radius:16px;padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--20);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--20)"><!-- wp:paragraph {"align":"center"} -->
+<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|50","right":"var:preset|spacing|50"}},"border":{"radius":"16px"}},"backgroundColor":"base-2","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide has-base-2-background-color has-background" style="border-radius:16px;padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)"><!-- wp:paragraph {"align":"center"} -->
 <p class="has-text-align-center"><?php echo esc_html_x( 'We’ve worked with some of the best companies.', 'Sample content above the list of companies.', 'twentytwentyfour' ); ?></p>
 <!-- /wp:paragraph -->
 
@@ -15,25 +15,26 @@
 <div style="height:var(--wp--preset--spacing--30)" aria-hidden="true" class="wp-block-spacer"></div>
 <!-- /wp:spacer -->
 
-<!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"center"}} -->
-<div class="wp-block-group alignwide"><!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
-<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/client-logo-1.webp" alt="<?php echo esc_attr( 'Logo of client 1', 'twentytwentyfour' ); ?>"/></figure>
+<!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"var:preset|spacing|30","padding":{"right":"0","left":"0","top":"0","bottom":"0"}}},"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"center"}} -->
+<div class="wp-block-group alignwide" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0">
+<!-- wp:image {"width":140,"height":65,"scale":"contain","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full is-resized"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/client-logo-1.webp" alt="<?php echo esc_attr( 'Logo of client 1', 'twentytwentyfour' ); ?>" style="object-fit:contain;width:140px;height:65px" width="140" height="65"/></figure>
 <!-- /wp:image -->
 
-<!-- wp:image {sizeSlug":"full","linkDestination":"none"} -->
-<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/client-logo-2.webp" alt="<?php echo esc_attr( 'Logo of client 2', 'twentytwentyfour' ); ?>"/></figure>
+<!-- wp:image {"width":110,"height":60,"scale":"contain","sizeSlug":"full","linkDestination":"none","className":"size-full"} -->
+<figure class="wp-block-image size-full is-resized"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/client-logo-2.webp" alt="<?php echo esc_attr( 'Logo of client 2', 'twentytwentyfour' ); ?>" style="object-fit:contain;width:110px;height:60px" width="110" height="60"/></figure>
 <!-- /wp:image -->
 
-<!-- wp:image {sizeSlug":"full","linkDestination":"none"} -->
-<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/client-logo-3.webp" alt="<?php echo esc_attr( 'Logo of client 3', 'twentytwentyfour' ); ?>"/></figure>
+<!-- wp:image {"width":150,"height":72,"scale":"contain","sizeSlug":"full","linkDestination":"none","className":"size-full"} -->
+<figure class="wp-block-image size-full is-resized"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/client-logo-3.webp" alt="<?php echo esc_attr( 'Logo of client 3', 'twentytwentyfour' ); ?>" style="object-fit:contain;width:150px;height:72px" width="150" height="72"/></figure>
 <!-- /wp:image -->
 
-<!-- wp:image {sizeSlug":"full","linkDestination":"none"} -->
-<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/client-logo-4.webp" alt="<?php echo esc_attr( 'Logo of client 4', 'twentytwentyfour' ); ?>"/></figure>
+<!-- wp:image {"width":160,"height":44,"scale":"contain","sizeSlug":"full","linkDestination":"none","className":"size-full"} -->
+<figure class="wp-block-image size-full is-resized"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/client-logo-4.webp" alt="<?php echo esc_attr( 'Logo of client 4', 'twentytwentyfour' ); ?>" style="object-fit:contain;width:160px;height:44px" width="160" height="44"/></figure>
 <!-- /wp:image -->
 
-<!-- wp:image {sizeSlug":"full","linkDestination":"none"} -->
-<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/client-logo-5.webp" alt="<?php echo esc_attr( 'Logo of client 5', 'twentytwentyfour' ); ?>"/></figure>
+<!-- wp:image {"width":184,"height":51,"scale":"contain","sizeSlug":"full","linkDestination":"none","className":"size-full"} -->
+<figure class="wp-block-image size-full is-resized"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/client-logo-5.webp" alt="<?php echo esc_attr( 'Logo of client 5', 'twentytwentyfour' ); ?>" style="object-fit:contain;width:184px;height:51px" width="184" height="51"/></figure>
 <!-- /wp:image --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
