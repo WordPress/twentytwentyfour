d601a0cec5d1c8ce1c2d111ae1efd9793373f0b9
shail-mehta <shailmehta25@gmail.com>
2023-09-08 21:27:26 +0530

    Updated Feature Grid Pattern

--- patterns/feature-grid.php
+++ patterns/feature-grid.php
@@ -117,7 +117,7 @@
 <!-- /wp:paragraph -->
 
 <!-- wp:paragraph {"align":"left"} -->
-<p class="has-text-align-left"><?php echo esc_html_x( 'Experience the fusion of imagination and expertise with Études Architectural Solutions.     ', 'Sample feature content', 'twentytwentyfour' ); ?></p>
+<p class="has-text-align-left"><?php echo esc_html_x( 'Experience the fusion of imagination and expertise with Études Architectural Solutions.', 'Sample feature content', 'twentytwentyfour' ); ?></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:column --></div>
 <!-- /wp:columns --></div>
