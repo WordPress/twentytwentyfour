e19047c4e42406952b126f916d1b015169ead16b
Carolina Nymark <hi@themesbycarolina.com>
2023-09-11 13:48:30 +0200

    Add image file and update markup

--- patterns/rsvp.php
+++ patterns/rsvp.php
@@ -6,24 +6,24 @@
  */
 ?>
 
-<!-- wp:media-text {"align":"full","mediaPosition":"right","style":{"spacing":{"padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40","left":"var:preset|spacing|40","right":"var:preset|spacing|40"}}},"backgroundColor":"accent-4"} -->
-<div class="wp-block-media-text alignfull has-media-on-the-right is-stacked-on-mobile has-accent-4-background-color has-background" style="padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--40)"><div class="wp-block-media-text__content">
-	<!-- wp:group {"layout":{"type":"default"}} -->
-	<div class="wp-block-group">
-		<!-- wp:paragraph {"align":"right","style":{"typography":{"writingMode":"vertical-rl"}},"fontSize":"xx-large","fontFamily":"cardo"} -->
-		<p class="has-text-align-right has-cardo-font-family has-xx-large-font-size" style="writing-mode:vertical-rl"><?php echo esc_html_x( 'RSVP', 'Initials for ´please respond´', 'twentytwentyfour' ); ?></p>
-		<!-- /wp:paragraph -->
+<!-- wp:media-text {"align":"full","mediaPosition":"right","mediaLink":"","mediaType":"image","verticalAlignment":"top","imageFill":true,"style":{"spacing":{"padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40","left":"var:preset|spacing|40","right":"var:preset|spacing|40"}}},"backgroundColor":"accent-4"} -->
+<div class="wp-block-media-text alignfull has-media-on-the-right is-stacked-on-mobile is-vertically-aligned-top is-image-fill has-accent-4-background-color has-background" style="padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--40)"><div class="wp-block-media-text__content"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|60"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch","verticalAlignment":"top"}} -->
+<div class="wp-block-group"><!-- wp:group {"layout":{"type":"default"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"align":"right","style":{"typography":{"writingMode":"vertical-rl","fontSize":"9rem"}},"fontFamily":"cardo"} -->
+<p class="has-text-align-right has-cardo-font-family" style="font-size:9rem;writing-mode:vertical-rl"><?php echo esc_html_x( 'RSVP', 'Initials for ´please respond´', 'twentytwentyfour' ); ?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
 
-		<!-- wp:paragraph {"fontSize":"small"} -->
-		<p class="has-small-font-size"><?php echo esc_html_x( 'Experience the fusion of imagination and expertise with Études Arch Summit, February 2024.', 'RSVP call to action description', 'twentytwentyfour' ); ?></p>
-		<!-- /wp:paragraph -->
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"fontSize":"small"} -->
+<p class="has-small-font-size"><?php echo esc_html_x( 'Experience the fusion of imagination and expertise with Études Arch Summit, February 2024.', 'RSVP call to action description', 'twentytwentyfour' ); ?></p>
+<!-- /wp:paragraph -->
 
-		<!-- wp:buttons -->
-		<div class="wp-block-buttons"><!-- wp:button -->
-		<div class="wp-block-button"><a class="wp-block-button__link wp-element-button"><?php echo esc_html_x( 'RSVP', 'Initials for ´please respond´', 'twentytwentyfour' ); ?></a></div>
-		<!-- /wp:button --></div>
-		<!-- /wp:buttons --></div>
-	<!-- /wp:group -->
-	</div>
-<figure class="wp-block-media-text__media"></figure></div>
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button"><?php echo esc_html_x( 'RSVP', 'Initials for ´please respond´', 'twentytwentyfour' ); ?></a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div><figure class="wp-block-media-text__media" style="background-image:url(<?php echo esc_url( get_template_directory_uri()); ?>/assets/images/rsvp.webp);background-position:50% 50%"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/rsvp.webp" alt="<?php echo esc_attr( 'An abstract Pattern Image', 'twentytwentyfour' ); ?>"/></figure></div>
 <!-- /wp:media-text -->
