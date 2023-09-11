55d94cd4ca3d111cddd092bf72bf201ee63bffa8
Rich Tabor <hi@richtabor.com>
2023-09-11 11:46:47 -0400

    Create template-portfolio-home.php

---
+++ patterns/template-portfolio-home.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Title: Portfolio Template
+ * Slug: twentytwentyfour/template-portfolio-home
+ * Template Types: front-page, index
+ * Viewport width: 1400
+ * Inserter: no
+ */
+?>
+
+<!-- wp:template-part {"slug":"header-portfolio","theme":"twentytwentyfour","area":"header"} /-->
+
+<!-- wp:pattern {"slug":"twentytwentyfour/hidden-intro-text-left"} /-->
+<!-- wp:pattern {"slug":"twentytwentyfour/offset-grid-image-posts"} /-->
+
+<!-- wp:template-part {"slug":"footer","theme":"twentytwentyfour","area":"footer"} /-->
