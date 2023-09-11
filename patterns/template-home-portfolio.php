e5ac2974fdc534156a16d52fbcb706613087d430
Rich Tabor <hi@richtabor.com>
2023-09-11 12:26:55 -0400

    Add portfolio template type (#322)
    * Create template-portfolio-home.php
    
    * Create footer-portfolio.html
    
    * Delete home-portfolio.html
    
    * Add template pattern for portfolio
    
    * Rename template, add home type

---
+++ patterns/template-home-portfolio.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Title: Portfolio Template
+ * Slug: twentytwentyfour/template-home-portfolio
+ * Template Types: front-page, index, home
+ * Viewport width: 1400
+ * Inserter: no
+ */
+?>
+
+<!-- wp:template-part {"slug":"header-portfolio","theme":"twentytwentyfour","area":"header"} /-->
+
+<!-- wp:pattern {"slug":"twentytwentyfour/hidden-intro-text-left"} /-->
+<!-- wp:pattern {"slug":"twentytwentyfour/offset-grid-image-posts"} /-->
+
+<!-- wp:template-part {"slug":"footer-portfolio","theme":"twentytwentyfour","area":"footer"} /-->
