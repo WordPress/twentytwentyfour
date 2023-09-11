aef4cfb120c83cef127434e1a5c0b3723bb939ad
MaggieCabrera <maggie.cabrera@automattic.com>
2023-09-11 12:39:08 +0200

    used presets

--- patterns/footer-writer.php
+++ patterns/footer-writer.php
@@ -6,14 +6,14 @@
  * Block Types: core/template-part/footer
  */
 ?>
+<!-- wp:group {"tagName":"footer","style":{"spacing":{"padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|50"}}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+<footer class="wp-block-group" style="padding-top:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--50)">
 
-<!-- wp:group {"tagName":"footer","style":{"spacing":{"padding":{"top":"5vh","bottom":"8vh"}}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
-<footer class="wp-block-group" style="padding-top:5vh;padding-bottom:8vh">
 <!-- wp:site-logo /-->
 
-<!-- wp:navigation {"overlayMenu":"never","layout":{"type":"flex","justifyContent":"center"},"style":{"typography":{"fontSize":"0.9rem"}}} /-->
+<!-- wp:navigation {"overlayMenu":"never","layout":{"type":"flex","justifyContent":"center"},"fontSize":"small"} /-->
 
-<!-- wp:paragraph {"align":"center","style":{"typography":{"fontSize":"0.8rem"},"elements":{"link":{"color":{"text":"var:preset|color|contrast"}}}},"textColor":"secondary"} -->
-<p class="has-text-align-center has-secondary-color has-text-color has-link-color" style="font-size:0.8rem">Designed with <a href="http://wordpress.org" data-type="link" data-id="wordpress.org">WordPress</a></p>
+<!-- wp:paragraph {"align":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|contrast"}}}},"textColor":"secondary","fontSize":"small"} -->
+<p class="has-text-align-center has-secondary-color has-text-color has-link-color has-small-font-size">Designed with <a href="http://wordpress.org" data-type="link" data-id="wordpress.org">WordPress</a></p>
 <!-- /wp:paragraph --></footer>
 <!-- /wp:group -->
