122cec65336a3c070aa3e7eee7853d47e12e1fab
MaggieCabrera <maggie.cabrera@automattic.com>
2023-09-12 11:55:26 +0200

    removes the template and adds the pattern

--- patterns/about.php
+++ patterns/about.php
@@ -7,6 +7,9 @@
 
 ?>
 
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group">
+
 <!-- wp:paragraph {"align":"center","style":{"typography":{"fontStyle":"italic","fontWeight":"400","lineHeight":"1.3"},"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"backgroundColor":"base","fontSize":"large","fontFamily":"cardo"} -->
 <p class="has-text-align-center has-base-background-color has-background has-cardo-font-family has-large-font-size" style="padding-top:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);font-style:italic;font-weight:400;line-height:1.3">
 	<?php
@@ -20,3 +23,6 @@
 		?>
 </p>
 <!-- /wp:paragraph -->
+
+</div>
+<!-- /wp:group -->
