bbb39371046651a8bf48d07ed5a014f296e2d8e2
Carolina Nymark <hi@themesbycarolina.com>
2023-09-12 07:03:43 +0200

    Restore the previous font size

--- patterns/posts.php
+++ patterns/posts.php
@@ -9,8 +9,8 @@
 
 <!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|50","right":"var:preset|spacing|50"},"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained"}} -->
 <div class="wp-block-group alignfull" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)">
-<!-- wp:heading {"level":1,"align":"wide","style":{"typography":{"lineHeight":"1"},"spacing":{"margin":{"top":"0"}}}} -->
-<h1 class="wp-block-heading alignwide" style="margin-top:0;line-height:1">Watch, Read, Listen</h1>
+<!-- wp:heading {"level":1,"align":"wide","style":{"typography":{"lineHeight":"1"},"spacing":{"margin":{"top":"0"}}},"fontSize":"x-large"} -->
+<h1 class="wp-block-heading alignwide has-x-large-font-size" style="margin-top:0;line-height:1">Watch, Read, Listen</h1>
 <!-- /wp:heading -->
 
 <!-- wp:spacer {"height":"var:preset|spacing|10","style":{"spacing":{"margin":{"top":"0","bottom":"0"}}}} -->
