5402cac9e19e723670f2ff221572ddd0539f5c51
Rich Tabor <hi@richtabor.com>
2023-09-13 21:04:14 -0400

    Remove duplicate header tags

--- patterns/template-home-portfolio.php
+++ patterns/template-home-portfolio.php
@@ -2,15 +2,17 @@
 /**
  * Title: Portfolio Template
  * Slug: twentytwentyfour/template-home-portfolio
- * Template Types: front-page, index, home
+ * Template Types: front-page, index, home, page
  * Viewport width: 1400
  * Inserter: no
  */
 ?>
 
-<!-- wp:group {"style":{"position":{"type":"sticky","top":"0px"}},"layout":{"type":"default"},"tagName":"header"} --><header class="wp-block-group">
+<!-- wp:group {"style":{"position":{"type":"sticky","top":"0px"}},"layout":{"type":"default"}} -->
+<div class="wp-block-group">
 	<!-- wp:template-part {"slug":"header","theme":"twentytwentyfour"} /-->
-</header><!-- /wp:group -->
+</div>
+<!-- /wp:group -->
 
 <!-- wp:pattern {"slug":"twentytwentyfour/hidden-intro-text-left"} /-->
 <!-- wp:pattern {"slug":"twentytwentyfour/offset-grid-image-posts"} /-->
