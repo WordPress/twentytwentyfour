5d2d52bba17cd1052c4e38c004c7927d79537347
Carolina Nymark <hi@themesbycarolina.com>
2023-09-16 03:37:24 +0200

    Update search.html and adjust spacing

--- patterns/archive.php
+++ patterns/archive.php
@@ -12,15 +12,15 @@
 	<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|50","right":"var:preset|spacing|50"},"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"default"}} -->
 	<div class="wp-block-group" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)">
 		<!-- wp:post-template {"align":"full","style":{"spacing":{"blockGap":"var:preset|spacing|40"}},"layout":{"type":"grid","columnCount":3}} -->
-			<!-- wp:post-featured-image {"isLink":true,"aspectRatio":"2/3"} /-->
-			<!-- wp:spacer {"height":"var:preset|spacing|20","style":{"spacing":{"margin":{"top":"0","bottom":"0"}}}} -->
-			<div style="margin-top:0;margin-bottom:0;height:var(--wp--preset--spacing--20)" aria-hidden="true" class="wp-block-spacer"></div>
-			<!-- /wp:spacer -->
+			<!-- wp:post-featured-image {"isLink":true,"aspectRatio":"2/3","style":{"spacing":{"margin":{"bottom":"var:preset|spacing|20"}}}} /-->
 			<!-- wp:group {"style":{"spacing":{"blockGap":"8px"}},"layout":{"type":"flex","orientation":"vertical","flexWrap":"nowrap"}} -->
 			<div class="wp-block-group">
 				<!-- wp:post-title {"isLink":true,"fontSize":"large"} /-->
 				<!-- wp:pattern {"slug":"twentytwentyfour/post-meta"} /-->
 				<!-- wp:post-excerpt {"fontSize":"small"} /-->
+				<!-- wp:spacer {"height":"var:preset|spacing|30"} -->
+				<div style="height:var(--wp--preset--spacing--30)" aria-hidden="true" class="wp-block-spacer"></div>
+				<!-- /wp:spacer -->
 			</div>
 			<!-- /wp:group -->
 		<!-- /wp:post-template -->
