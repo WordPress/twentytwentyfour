2d0df4c4e1b64c7bf3e8ddd6b8a41a6dca86d355
Carolina Nymark <hi@themesbycarolina.com>
2023-09-17 20:55:14 +0200

    Add group between query and post template

--- patterns/archive.php
+++ patterns/archive.php
@@ -9,38 +9,43 @@
 
 <!-- wp:query {"query":{"perPage":10,"pages":0,"offset":"0","postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":true},"align":"wide","layout":{"type":"default"}} -->
 <div class="wp-block-query alignwide">
-	<!-- wp:post-template {"align":"full","style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"layout":{"type":"grid","columnCount":3}} -->
+	<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"0","right":"0"},"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"default"}} -->
+	<div class="wp-block-group" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--50);padding-right:0;padding-bottom:var(--wp--preset--spacing--50);padding-left:0">
 
-		<!-- wp:post-featured-image {"isLink":true,"aspectRatio":"2/3","style":{"spacing":{"margin":{"bottom":"0"},"padding":{"bottom":"var:preset|spacing|20"}}}} /-->
+		<!-- wp:post-template {"align":"full","style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"layout":{"type":"grid","columnCount":3}} -->
 
-		<!-- wp:group {"style":{"spacing":{"blockGap":"8px"}},"layout":{"type":"flex","orientation":"vertical","flexWrap":"nowrap"}} -->
-		<div class="wp-block-group">
-			<!-- wp:post-title {"isLink":true,"fontSize":"large"} /-->
+			<!-- wp:post-featured-image {"isLink":true,"aspectRatio":"2/3","style":{"spacing":{"margin":{"bottom":"0"},"padding":{"bottom":"var:preset|spacing|20"}}}} /-->
 
-			<!-- wp:pattern {"slug":"twentytwentyfour/post-meta"} /-->
+			<!-- wp:group {"style":{"spacing":{"blockGap":"8px"}},"layout":{"type":"flex","orientation":"vertical","flexWrap":"nowrap"}} -->
+			<div class="wp-block-group">
+				<!-- wp:post-title {"isLink":true,"fontSize":"large"} /-->
 
-			<!-- wp:post-excerpt {"textColor":"contrast-2","fontSize":"small"} /-->
+				<!-- wp:pattern {"slug":"twentytwentyfour/post-meta"} /-->
 
-			<!-- wp:spacer {"height":"var:preset|spacing|30"} -->
-			<div style="height:var(--wp--preset--spacing--30)" aria-hidden="true" class="wp-block-spacer"></div>
-			<!-- /wp:spacer -->
-		</div>
-		<!-- /wp:group -->
+				<!-- wp:post-excerpt {"textColor":"contrast-2","fontSize":"small"} /-->
 
-	<!-- /wp:post-template -->
+				<!-- wp:spacer {"height":"var:preset|spacing|30"} -->
+				<div style="height:var(--wp--preset--spacing--30)" aria-hidden="true" class="wp-block-spacer"></div>
+				<!-- /wp:spacer -->
+			</div>
+			<!-- /wp:group -->
 
-	<!-- wp:spacer {"height":"var:preset|spacing|40","style":{"spacing":{"margin":{"top":"0","bottom":"0"}}}} -->
-	<div style="margin-top:0;margin-bottom:0;height:var(--wp--preset--spacing--40)" aria-hidden="true" class="wp-block-spacer"></div>
-	<!-- /wp:spacer -->
+		<!-- /wp:post-template -->
 
-	<!-- wp:query-pagination {"paginationArrow":"arrow","layout":{"type":"flex","justifyContent":"space-between"}} -->
-	<!-- wp:query-pagination-previous /-->
-	<!-- wp:query-pagination-next /-->
-	<!-- /wp:query-pagination -->
+		<!-- wp:spacer {"height":"var:preset|spacing|40","style":{"spacing":{"margin":{"top":"0","bottom":"0"}}}} -->
+		<div style="margin-top:0;margin-bottom:0;height:var(--wp--preset--spacing--40)" aria-hidden="true" class="wp-block-spacer"></div>
+		<!-- /wp:spacer -->
 
-	<!-- wp:query-no-results -->
-		<!-- wp:pattern {"slug":"twentytwentyfour/hidden-no-results-content"} /-->
-	<!-- /wp:query-no-results -->
+		<!-- wp:query-pagination {"paginationArrow":"arrow","layout":{"type":"flex","justifyContent":"space-between"}} -->
+		<!-- wp:query-pagination-previous /-->
+		<!-- wp:query-pagination-next /-->
+		<!-- /wp:query-pagination -->
 
+		<!-- wp:query-no-results -->
+			<!-- wp:pattern {"slug":"twentytwentyfour/hidden-no-results-content"} /-->
+		<!-- /wp:query-no-results -->
+
+	</div>
+	<!-- /wp:group -->
 </div>
 <!-- /wp:query -->
