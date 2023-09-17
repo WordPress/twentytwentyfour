74ff07769cec27fe7fefc71ab9719370805f7ec0
Carolina Nymark <hi@themesbycarolina.com>
2023-09-17 20:27:05 +0200

    Update archive.php

--- patterns/archive.php
+++ patterns/archive.php
@@ -10,13 +10,13 @@
 <!-- wp:query {"query":{"perPage":10,"pages":0,"offset":"0","postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":true},"align":"wide","layout":{"type":"default"}} -->
 <div class="wp-block-query alignwide">
 	<!-- wp:post-template {"align":"full","style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"layout":{"type":"grid","columnCount":3}} -->
-	
+
 		<!-- wp:post-featured-image {"isLink":true,"aspectRatio":"auto","style":{"spacing":{"margin":{"bottom":"0"},"padding":{"bottom":"var:preset|spacing|20"}}}} /-->
 
 		<!-- wp:group {"style":{"spacing":{"blockGap":"8px"}},"layout":{"type":"flex","orientation":"vertical","flexWrap":"nowrap"}} -->
 		<div class="wp-block-group">
 			<!-- wp:post-title {"isLink":true,"fontSize":"large"} /-->
-	
+
 			<!-- wp:pattern {"slug":"twentytwentyfour/post-meta"} /-->
 
 			<!-- wp:post-excerpt {"textColor":"contrast-2","fontSize":"small"} /-->
