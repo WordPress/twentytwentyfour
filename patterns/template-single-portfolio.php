6cfe3f7619f8ab0d3a4dfae0a4ba80ed3ff93816
Rich Tabor <hi@richtabor.com>
2023-09-18 13:03:38 -0400

    Use as an alternate template for pages/posts

--- patterns/template-single-portfolio.php
+++ patterns/template-single-portfolio.php
@@ -1,21 +1,33 @@
+<?php
+/**
+ * Title: Portfolio Single Template
+ * Slug: twentytwentyfour/template-single-portfolio
+ * Template Types: page, single
+ * Viewport width: 1400
+ * Inserter: no
+ */
+?>
+
 <!-- wp:group {"style":{"position":{"type":"sticky","top":"0px"}},"layout":{"type":"default"}} -->
 <div class="wp-block-group">
-	<!-- wp:template-part {"slug":"header"} /-->
+	<!-- wp:template-part {"slug":"header","theme":"twentytwentyfour"} /-->
 </div>
 <!-- /wp:group -->
 
 <!-- wp:group {"tagName":"main","align":"full","layout":{"type":"constrained"}} -->
-<main class="wp-block-group alignfull"><!-- wp:spacer {"height":"var:preset|spacing|40"} -->
+<main class="wp-block-group alignfull">
+
+<!-- wp:spacer {"height":"var:preset|spacing|40"} -->
 	<div style="height:var(--wp--preset--spacing--40)" aria-hidden="true" class="wp-block-spacer"></div>
 	<!-- /wp:spacer -->
-	
+
 	<!-- wp:post-featured-image {"align":"wide","style":{"spacing":{"margin":{"bottom":"var:preset|spacing|20"}}}} /-->
-	
+
 	<!-- wp:columns {"align":"wide"} -->
 	<div class="wp-block-columns alignwide"><!-- wp:column {"width":"45%"} -->
 	<div class="wp-block-column" style="flex-basis:45%"></div>
 	<!-- /wp:column -->
-	
+
 	<!-- wp:column {"width":"55%"} -->
 	<div class="wp-block-column" style="flex-basis:55%"><!-- wp:pattern {"slug":"twentytwentyfour/post-meta"} /--></div>
 	<!-- /wp:column --></div>
@@ -28,4 +40,4 @@
 </main>
 <!-- /wp:group -->
 
-<!-- wp:template-part {"slug":"footer-portfolio","area":"footer","tagName":"footer"} /-->
+<!-- wp:template-part {"slug":"footer","area":"footer","tagName":"footer","theme":"twentytwentyfour"} /-->
