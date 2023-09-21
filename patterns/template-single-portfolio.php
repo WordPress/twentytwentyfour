9cc1eda77a342d92cd55d384b8696ef2e7f7910a
Jordan <jordan@jordesign.com>
2023-09-21 10:55:04 +1000

    Use correct Footer template part for single-portfolio template

--- patterns/template-single-portfolio.php
+++ patterns/template-single-portfolio.php
@@ -38,4 +38,5 @@
 </main>
 <!-- /wp:group -->
 
-<!-- wp:template-part {"slug":"footer-portfolio","area":"footer","tagName":"footer"} /-->
+<!-- wp:template-part {"slug":"footer","area":"footer","tagName":"footer"} /-->
+
