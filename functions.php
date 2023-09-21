871ab24cadbe5b726fdcf4dde8437ed955a4a7f4
MaggieCabrera <maggie.cabrera@automattic.com>
2023-09-21 16:13:27 +0200

    linting

--- functions.php
+++ functions.php
@@ -86,8 +86,8 @@ if ( ! function_exists( 'twentytwentyfour_block_styles' ) ) :
 		register_block_style(
 			'core/heading',
 			array(
-				'name'       => 'asterisk',
-				'label'      => __( 'With asterisk', 'twentytwentyfour' ),
+				'name'         => 'asterisk',
+				'label'        => __( 'With asterisk', 'twentytwentyfour' ),
 				/* TODO: RTL considerations */
 				'inline_style' => ".is-style-asterisk:before{content:'';width: 1.5rem;height:3rem;background: var(--wp--preset--color--contrast-2);
 					clip-path: path('M11.93.684v8.039l5.633-5.633 1.216 1.23-5.66 5.66h8.04v1.737H13.2l5.701 5.701-1.23 1.23-5.742-5.742V21h-1.737v-8.094l-5.77 5.77-1.23-1.217 5.743-5.742H.842V9.98h8.162l-5.701-5.7 1.23-1.231 5.66 5.66V.684h1.737Z'); display: block;}.is-style-asterisk.has-text-align-center:before{margin: 0 auto;}.is-style-asterisk.has-text-align-right:before{margin-left: auto;}.rtl .is-style-asterisk.has-text-align-left:before{margin-right: auto;}",
