306c588aa54be35dd943e5bc27f2d157639d59ae
scruffian <ben@scruffian.com>
2023-09-22 16:24:13 +0100

    Make the default home page a pattern so it can be switched back to

---
+++ patterns/template-home.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Title: Home Template
+ * Slug: twentytwentyfour/template-home
+ * Template Types: front-page, index, home
+ * Viewport width: 1400
+ * Inserter: no
+ */
+?>
+
+<!-- wp:group {"style":{"position":{"type":"sticky","top":"0px"}},"layout":{"type":"default"}} -->
+<div class="wp-block-group">
+		<!-- wp:template-part {"slug":"header"} /-->
+	</div>
+<!-- /wp:group -->
+
+<!-- wp:group {"tagName":"main","style":{"spacing":{"blockGap":"0","margin":{"top":"0"}}},"layout":{"type":"default"}} -->
+<main class="wp-block-group" style="margin-top:0">
+	<!-- wp:pattern {"slug":"twentytwentyfour/home"} /-->
+</main>
+<!-- /wp:group -->
+
+<!-- wp:template-part {"slug":"footer","area":"footer","tagName":"footer"} /-->
