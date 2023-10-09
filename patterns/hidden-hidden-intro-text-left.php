74d8e1962e828b9c8c8f9151ca416076556ad47c
MaggieCabrera <maggie.cabrera@automattic.com>
2023-10-09 17:34:24 +0200

    first pass at formatting

--- patterns/hidden-hidden-intro-text-left.php
+++ patterns/hidden-hidden-intro-text-left.php
@@ -13,17 +13,25 @@
 
 <!-- wp:group {"align":"wide","layout":{"type":"constrained"}} -->
 <div class="wp-block-group alignwide">
-<!-- wp:columns {"align":"wide"} -->
-<div class="wp-block-columns alignwide"><!-- wp:column {"width":"70%"} -->
-<div class="wp-block-column" style="flex-basis:70%">
+	<!-- wp:columns {"align":"wide"} -->
+	<div class="wp-block-columns alignwide">
+		<!-- wp:column {"width":"70%"} -->
+		<div class="wp-block-column" style="flex-basis:70%">
 
-<!-- wp:heading {"level":1,"style":{"typography":{"lineHeight":"1.2"}},"fontSize":"xx-large"} --><h1 class="wp-block-heading has-xx-large-font-size" style="line-height:1.2"><?php echo wp_kses_post( __( 'I’m <em>Leia Acosta</em>, a passionate photographer who finds inspiration in capturing the fleeting beauty of life.' ) ); ?></h1><!-- /wp:heading -->
+			<!-- wp:heading {"level":1,"style":{"typography":{"lineHeight":"1.2"}},"fontSize":"xx-large"} -->
+			<h1 class="wp-block-heading has-xx-large-font-size" style="line-height:1.2">
+				<?php echo wp_kses_post(__('I’m <em>Leia Acosta</em>, a passionate photographer who finds inspiration in capturing the fleeting beauty of life.')); ?>
+			</h1>
+			<!-- /wp:heading -->
 
+		</div>
+		<!-- /wp:column -->
+
+		<!-- wp:column -->
+		<div class="wp-block-column">
+		</div>
+		<!-- /wp:column -->
+	</div>
+	<!-- /wp:columns -->
 </div>
-<!-- /wp:column -->
-
-<!-- wp:column -->
-<div class="wp-block-column"></div>
-<!-- /wp:column --></div>
-<!-- /wp:columns --></div>
 <!-- /wp:group -->
