2406f7f56ad482223532ae07ba1dcd64916bf7e5
Oliver Juhas <webmandesign@users.noreply.github.com>
2023-10-10 13:51:02 +0200

    Simplifying HTML of the pattern

--- patterns/hidden-hidden-intro-text-left.php
+++ patterns/hidden-hidden-intro-text-left.php
@@ -11,19 +11,10 @@
 <div style="height:var(--wp--preset--spacing--50)" aria-hidden="true" class="wp-block-spacer"></div>
 <!-- /wp:spacer -->
 
-<!-- wp:group {"align":"wide","layout":{"type":"constrained"}} -->
+<!-- wp:group {"align":"wide","layout":{"type":"constrained","justifyContent":"left","contentSize":"890px"}} -->
 <div class="wp-block-group alignwide">
-<!-- wp:columns {"align":"wide"} -->
-<div class="wp-block-columns alignwide"><!-- wp:column {"width":"70%"} -->
-<div class="wp-block-column" style="flex-basis:70%">
-
-<!-- wp:heading {"level":1,"style":{"typography":{"lineHeight":"1.2"}},"fontSize":"xx-large"} --><h1 class="wp-block-heading has-xx-large-font-size" style="line-height:1.2"><?php echo wp_kses_post( __( 'I’m <em>Leia Acosta</em>, a passionate photographer who finds inspiration in capturing the fleeting beauty of life.' ) ); ?></h1><!-- /wp:heading -->
-
+	<!-- wp:heading {"level":1,"style":{"typography":{"lineHeight":"1.2"}},"fontSize":"xx-large"} -->
+	<h1 class="wp-block-heading has-xx-large-font-size" style="line-height:1.2"><?php echo wp_kses_post( __( 'I’m <em>Leia Acosta</em>, a passionate photographer who finds inspiration in capturing the fleeting beauty of life.' ) ); ?></h1>
+	<!-- /wp:heading -->
 </div>
-<!-- /wp:column -->
-
-<!-- wp:column -->
-<div class="wp-block-column"></div>
-<!-- /wp:column --></div>
-<!-- /wp:columns --></div>
 <!-- /wp:group -->
