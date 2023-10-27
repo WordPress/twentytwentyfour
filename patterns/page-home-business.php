e02a2e78e56c9d4e9a14ee6a5871b13eaac9ec07
Jessica Lyschik <luminuu@gmail.com>
2023-10-27 21:22:37 +0200

    Rename home to front-page, change pattern, update template slugs

--- patterns/page-home-business.php
+++ patterns/page-home-business.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Title: Business home
- * Slug: twentytwentyfour/home-business
+ * Slug: twentytwentyfour/page-home-business
  * Categories: page
  * Keywords: starter
  * Block Types: core/post-content
@@ -14,5 +14,13 @@
 <!-- wp:pattern {"slug":"twentytwentyfour/text-feature-grid-3-col"} /-->
 <!-- wp:pattern {"slug":"twentytwentyfour/text-alternating-images"} /-->
 <!-- wp:pattern {"slug":"twentytwentyfour/testimonial-centered"} /-->
-<!-- wp:pattern {"slug":"twentytwentyfour/posts-grid-2-col"} /-->
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|50","right":"var:preset|spacing|50"},"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)">
+	<!-- wp:heading {"align":"wide","style":{"typography":{"lineHeight":"1"},"spacing":{"margin":{"top":"0"}}},"fontSize":"x-large"} -->
+	<h2 class="wp-block-heading alignwide has-x-large-font-size" style="margin-top:0;line-height:1"><?php esc_html_e( 'Watch, Read, Listen', 'twentytwentyfour' ); ?></h2>
+	<!-- /wp:heading -->
+    
+    <!-- wp:pattern {"slug":"twentytwentyfour/posts-3-col"} /-->
+</div>
+<!-- /wp:group -->
 <!-- wp:pattern {"slug":"twentytwentyfour/cta-subscribe-centered"} /-->
